382e7095e84a1e70fe0ce216af052a7c7aebf293
Philipp Hoenisch <philipp@hoenisch.at>
2021-09-21 14:36:23 +1000

    Add logging library and initialize by default

--- daemon/README.md
+++ daemon/README.md
@@ -25,3 +25,16 @@ Currently the database for taker and maker is the same.
 The `taker` binary is used as an example to run the `prepare` command above, but it is irrelevant if you run it for taker or maker.
 The `tempdb` created can be deleted, it should not be checked into the repo.
 You can keep it around and just run the `prepare` statement multiple times when working on the database.
+
+## Logging
+
+`maker` and `taker` both make use of a log library which can be controlled using the environment variable `RUST_LOG`.
+By default, logging is set to `debug` for application specific logs only.
+If you want to see logs by third party libraries, you need to set `RUST_LOG=`.
+For example, to enable logs for rocket, use:
+
+```bash
+RUST_LOG=rocket=debug cargo run --bin taker
+```
+
+Additionally, logs can be formatted in json format by passing `--json` or `-j`.
---
+++ daemon/src/logger.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::time::ChronoLocal;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+pub fn init(level: LevelFilter, json_format: bool) -> Result<()> {
+    if level == LevelFilter::OFF {
+        return Ok(());
+    }
+
+    let is_terminal = atty::is(atty::Stream::Stderr);
+
+    let filter = EnvFilter::from_default_env()
+        .add_directive(format!("taker={}", level).parse()?)
+        .add_directive(format!("maker={}", level).parse()?);
+
+    let builder = FmtSubscriber::builder()
+        .with_env_filter(filter)
+        .with_writer(std::io::stderr)
+        .with_ansi(is_terminal)
+        .with_timer(ChronoLocal::with_format("%F %T".to_owned()))
+        .with_target(false);
+
+    if json_format {
+        builder.json().init();
+    } else {
+        builder.init();
+    }
+
+    tracing::info!("Initialized logger");
+
+    Ok(())
+}
