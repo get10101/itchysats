949704347862892471f3b44b3f322c2f370cc61a
Philipp Hoenisch <philipp@hoenisch.at>
2021-10-19 09:39:35 +1100

    Print git tag when starting maker/taker

--- daemon/build.rs
+++ daemon/build.rs
@@ -1,6 +1,12 @@
-fn main() -> std::io::Result<()> {
+use anyhow::Result;
+use vergen::{vergen, Config, SemverKind};
+
+fn main() -> Result<()> {
     std::fs::create_dir_all("../frontend/dist/maker")?;
     std::fs::create_dir_all("../frontend/dist/taker")?;
 
-    Ok(())
+    let mut config = Config::default();
+    *config.git_mut().semver_kind_mut() = SemverKind::Lightweight;
+
+    vergen(config)
 }
