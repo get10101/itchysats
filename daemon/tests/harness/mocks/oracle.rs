a5285ed51006510e7c31629cadde3b85098f31b8
Mariusz Klochowicz <mariusz@klochowicz.com>
2021-10-28 17:33:05 +1030

    Roll-out usage of a mocking framework in actor tests
    Mockall is a mocking framework that removes the need for writing more actors,
    making tests easier to write.
    
    Summary:
    - add one more layer of indirection (a trait per actor type: Wallet, Oracle, Monitor)
    - Mocks implementing the actor traits (with default stubbed implementations if no extra
      behaviour needed)
    - references to the mocks are being passed into the tests (via Arc<Mutex>>), allowing
      for dynamically changing the behaviour and adding assertions. This also
        aids readability, as the mock setup can be collocated with a particular
        test, if the test needs something extra
    
    Fixes #450

--- daemon/tests/harness/mocks/oracle.rs
+++ daemon/tests/harness/mocks/oracle.rs
@@ -1,45 +1,57 @@
+use crate::harness::cfd_protocol::OliviaData;
 use daemon::model::BitMexPriceEventId;
 use daemon::oracle;
+use mockall::*;
+use std::sync::Arc;
 use time::OffsetDateTime;
+use tokio::sync::Mutex;
 use xtra_productivity::xtra_productivity;
 
-pub struct Oracle {
-    announcement: Option<oracle::Announcement>,
+/// Test Stub simulating the Oracle actor.
+/// Serves as an entrypoint for injected mock handlers.
+pub struct OracleActor {
+    pub mock: Arc<Mutex<dyn Oracle + Send>>,
 }
 
-impl Oracle {
-    pub fn with_dummy_announcement(
-        mut self,
-        dummy_announcement: cfd_protocol::Announcement,
-    ) -> Self {
-        self.announcement = Some(oracle::Announcement {
-            id: BitMexPriceEventId::new(OffsetDateTime::UNIX_EPOCH, 0),
-            expected_outcome_time: OffsetDateTime::now_utc(),
-            nonce_pks: dummy_announcement.nonce_pks,
-        });
-
-        self
-    }
-}
-
-impl xtra::Actor for Oracle {}
+impl xtra::Actor for OracleActor {}
+impl Oracle for OracleActor {}
 
 #[xtra_productivity(message_impl = false)]
-impl Oracle {
-    async fn handle_get_announcement(
-        &mut self,
-        _msg: oracle::GetAnnouncement,
-    ) -> Option<oracle::Announcement> {
-        self.announcement.clone()
+impl OracleActor {
+    async fn handle(&mut self, msg: oracle::GetAnnouncement) -> Option<oracle::Announcement> {
+        self.mock.lock().await.get_announcement(msg)
     }
 
-    async fn handle(&mut self, _msg: oracle::MonitorAttestation) {}
+    async fn handle(&mut self, msg: oracle::MonitorAttestation) {
+        self.mock.lock().await.monitor_attestation(msg)
+    }
 
-    async fn handle(&mut self, _msg: oracle::Sync) {}
+    async fn handle(&mut self, msg: oracle::Sync) {
+        self.mock.lock().await.sync(msg)
+    }
 }
 
-impl Default for Oracle {
-    fn default() -> Self {
-        Oracle { announcement: None }
+#[automock]
+pub trait Oracle {
+    fn get_announcement(&mut self, _msg: oracle::GetAnnouncement) -> Option<oracle::Announcement> {
+        unreachable!("mockall will reimplement this method")
+    }
+
+    fn monitor_attestation(&mut self, _msg: oracle::MonitorAttestation) {
+        unreachable!("mockall will reimplement this method")
+    }
+
+    fn sync(&mut self, _msg: oracle::Sync) {
+        unreachable!("mockall will reimplement this method")
+    }
+}
+
+pub fn dummy_announcement() -> oracle::Announcement {
+    let announcement = OliviaData::example_0().announcement();
+
+    oracle::Announcement {
+        id: BitMexPriceEventId::new(OffsetDateTime::UNIX_EPOCH, 0),
+        expected_outcome_time: OffsetDateTime::now_utc(),
+        nonce_pks: announcement.nonce_pks,
     }
 }
