dd955d5ce7707675b85f19a97daae9e15fb833f3
Mariusz Klochowicz <mariusz@klochowicz.com>
2021-11-11 15:52:51 +1030

    Align vite configuration of the new taker UI
    - use vitejs/plugin-react-refresh
    - proxy `/alive` path

--- taker-frontend/vite.config.ts
+++ taker-frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        (
+            process.env.NODE_ENV !== "test"
+                ? [reactRefresh()]
+                : []
+        ),
+    ],
     build: {
         rollupOptions: {
             input: resolve(__dirname, `index.html`),
@@ -14,6 +20,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": "http://localhost:8000",
+            "/alive": "http://localhost:8000",
         },
     },
 });
