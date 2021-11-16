1e38ec38ac16807de3525a7de22e477e8702af60
Thomas Eizinger <thomas@eizinger.io>
2021-11-16 17:53:27 +1100

    `unwrap` only in the tests
    This makes the stacktrace more useful because it shows us where exactly
    it is failing.

--- daemon/tests/harness/flow.rs
+++ daemon/tests/harness/flow.rs
@@ -1,8 +1,7 @@
-use std::time::Duration;
-
-use anyhow::Context;
+use anyhow::{Context, Result};
 use daemon::model::cfd::{Cfd, Order};
 use daemon::tokio_ext::FutureExt;
+use std::time::Duration;
 use tokio::sync::watch;
 
 /// Returns the first `Cfd` from both channels
@@ -11,58 +10,54 @@ use tokio::sync::watch;
 pub async fn next_cfd(
     rx_a: &mut watch::Receiver<Vec<Cfd>>,
     rx_b: &mut watch::Receiver<Vec<Cfd>>,
-) -> (Cfd, Cfd) {
+) -> Result<(Cfd, Cfd)> {
     let (a, b) = tokio::join!(next(rx_a), next(rx_b));
+    let (a, b) = (a?, b?);
 
     assert_eq!(a.len(), 1);
     assert_eq!(b.len(), 1);
 
-    (a.first().unwrap().clone(), b.first().unwrap().clone())
+    Ok((a.first().unwrap().clone(), b.first().unwrap().clone()))
 }
 
 pub async fn next_order(
     rx_a: &mut watch::Receiver<Option<Order>>,
     rx_b: &mut watch::Receiver<Option<Order>>,
-) -> (Order, Order) {
+) -> Result<(Order, Order)> {
     let (a, b) = tokio::join!(next_some(rx_a), next_some(rx_b));
 
-    (a, b)
+    Ok((a?, b?))
 }
 
 /// Returns the value if the next Option received on the stream is Some
-///
-/// Panics if None is received on the stream.
-pub async fn next_some<T>(rx: &mut watch::Receiver<Option<T>>) -> T
+pub async fn next_some<T>(rx: &mut watch::Receiver<Option<T>>) -> Result<T>
 where
     T: Clone,
 {
-    if let Some(value) = next(rx).await {
-        value
-    } else {
-        panic!("Received None when Some was expected")
-    }
+    next(rx)
+        .await?
+        .context("Received None when Some was expected")
 }
 
 /// Returns true if the next Option received on the stream is None
 ///
 /// Returns false if Some is received.
-pub async fn is_next_none<T>(rx: &mut watch::Receiver<Option<T>>) -> bool
+pub async fn is_next_none<T>(rx: &mut watch::Receiver<Option<T>>) -> Result<bool>
 where
     T: Clone,
 {
-    next(rx).await.is_none()
+    Ok(next(rx).await?.is_none())
 }
 
 /// Returns watch channel value upon change
-pub async fn next<T>(rx: &mut watch::Receiver<T>) -> T
+pub async fn next<T>(rx: &mut watch::Receiver<T>) -> Result<T>
 where
     T: Clone,
 {
     rx.changed()
         .timeout(Duration::from_secs(10))
         .await
-        .context("Waiting for next element in channel is taking too long, aborting")
-        .unwrap()
-        .unwrap();
-    rx.borrow().clone()
+        .context("No change in channel within 10 seconds")??;
+
+    Ok(rx.borrow().clone())
 }
