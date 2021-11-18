e1da0b7f664bf5f56211acab8d9faa2c2ccdf43b
rishflab <rishflab@hotmail.com>
2021-11-18 20:38:36 +1100

    Extend contract setup happy path test to ensure cfd is open

--- daemon/tests/happy_path.rs
+++ daemon/tests/happy_path.rs
@@ -83,21 +83,6 @@ async fn taker_takes_order_and_maker_accepts_and_contract_setup() {
     taker.take_order(received.clone(), Usd::new(dec!(5))).await;
     let (_, _) = next_cfd(taker.cfd_feed(), maker.cfd_feed()).await.unwrap();
 
-    maker.mocks.mock_oracle_annoucement().await;
-    taker.mocks.mock_oracle_annoucement().await;
-
-    maker.mocks.mock_party_params().await;
-    taker.mocks.mock_party_params().await;
-
-    maker.mocks.mock_monitor_oracle_attestation().await;
-    taker.mocks.mock_monitor_oracle_attestation().await;
-
-    maker.mocks.mock_oracle_monitor_attestation().await;
-    taker.mocks.mock_oracle_monitor_attestation().await;
-
-    maker.mocks.mock_monitor_start_monitoring().await;
-    taker.mocks.mock_monitor_start_monitoring().await;
-
     maker.accept_take_request(received.clone()).await;
 
     let (taker_cfd, maker_cfd) = next_cfd(taker.cfd_feed(), maker.cfd_feed()).await.unwrap();
@@ -116,6 +101,109 @@ async fn taker_takes_order_and_maker_accepts_and_contract_setup() {
     assert_eq!(maker_cfd.order.id, received.id);
     assert!(matches!(taker_cfd.state, CfdState::PendingOpen { .. }));
     assert!(matches!(maker_cfd.state, CfdState::PendingOpen { .. }));
+
+    maker
+        .system
+        .cfd_actor_addr
+        .send(monitor::Event::LockFinality(maker_cfd.order.id))
+        .await
+        .unwrap();
+    taker
+        .system
+        .cfd_actor_addr
+        .send(monitor::Event::LockFinality(taker_cfd.order.id))
+        .await
+        .unwrap();
+
+    let (taker_cfd_open_state, maker_cfd_open_state) =
+        next_cfd(&mut taker.cfd_feed(), &mut maker.cfd_feed())
+            .await
+            .unwrap();
+
+    assert!(matches!(taker_cfd_open_state.state, CfdState::Open { .. }));
+    assert!(matches!(maker_cfd_open_state.state, CfdState::Open { .. }));
+}
+
+#[tokio::test]
+async fn taker_proposes_rollover_and_maker_accepts_rollover() -> Result<()> {
+    let _guard = init_tracing();
+    let (mut maker, mut taker) = start_both().await;
+    let (taker_cfd_open_state, maker_cfd_open_state) =
+        init_open_cfd(&mut maker, &mut taker).await?;
+
+    // First rollover
+    taker
+        .trigger_propose_rollover(taker_cfd_open_state.order.id)
+        .await;
+
+    let update_cfd_proposal = maker
+        .get_update_cfd_proposal(&maker_cfd_open_state.order.id)
+        .await
+        .expect("update cfd proposal received by maker");
+
+    assert!(matches!(
+        update_cfd_proposal,
+        UpdateCfdProposal::RollOverProposal { .. }
+    ));
+
+    maker.accept_rollover(maker_cfd_open_state.order).await;
+
+    let (taker_cfd_rolled_over_1, maker_cfd_rolled_over_1) =
+        next_cfd(&mut taker.cfd_feed(), &mut maker.cfd_feed()).await?;
+
+    assert!(matches!(
+        taker_cfd_rolled_over_1.state,
+        CfdState::Open { .. }
+    ));
+    assert!(matches!(
+        maker_cfd_rolled_over_1.state,
+        CfdState::Open { .. }
+    ));
+
+    assert_ne!(
+        taker_cfd_open_state.state.get_transition_timestamp(),
+        taker_cfd_rolled_over_1.state.get_transition_timestamp()
+    );
+
+    // We need to sleep for at least 1 second to ensure the rolled-over cfd has a different
+    // transition timestamp
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    // Second rollover to test consecutive rollovers
+    taker
+        .trigger_propose_rollover(taker_cfd_rolled_over_1.order.id)
+        .await;
+
+    let update_cfd_proposal = maker
+        .get_update_cfd_proposal(&maker_cfd_rolled_over_1.order.id)
+        .await
+        .expect("update cfd proposal received by maker");
+
+    assert!(matches!(
+        update_cfd_proposal,
+        UpdateCfdProposal::RollOverProposal { .. }
+    ));
+
+    maker.accept_rollover(maker_cfd_rolled_over_1.order).await;
+
+    let (taker_cfd_rolled_over_2, maker_cfd_rolled_over_2) =
+        next_cfd(&mut taker.cfd_feed(), &mut maker.cfd_feed()).await?;
+
+    assert!(matches!(
+        taker_cfd_rolled_over_2.state,
+        CfdState::Open { .. }
+    ));
+    assert!(matches!(
+        maker_cfd_rolled_over_2.state,
+        CfdState::Open { .. }
+    ));
+
+    assert_ne!(
+        taker_cfd_rolled_over_1.state.get_transition_timestamp(),
+        taker_cfd_rolled_over_2.state.get_transition_timestamp()
+    );
+
+    Ok(())
 }
 
 #[tokio::test]
