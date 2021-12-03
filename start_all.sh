876349f92a62e199e26e70f8992f448bcb9bf0a9
Thomas Eizinger <thomas@eizinger.io>
2021-12-03 12:33:47 +1100

    Introduce cargo aliases for running maker and taker in dev mode
    These can be used as:
    
    `cargo dev-taker`
    `cargo dev-maker`

--- start_all.sh
+++ start_all.sh
@@ -4,5 +4,5 @@ export RUST_BACKTRACE=1
 
 # A simple command to spin up the complete package, ie. both daemons and frontends.
 # A single 'ctrl+c' stops all processes.
-# The maker-id is generated from the makers seed found in daemon/util/testnet_seeds/maker_seed
-(trap 'kill 0' SIGINT; cargo run --bin maker -- testnet & cargo run --bin taker -- --maker localhost:9999 --maker-id 10d4ba2ac3f7a22da4009d813ff1bc3f404dfe2cc93a32bedf1512aa9951c95e testnet -- & yarn --cwd=./maker-frontend dev  & yarn --cwd=./taker-frontend dev)
+# The maker-id is generated from the makers seed found in daemon/testnet/maker_seed
+(trap 'kill 0' SIGINT; cargo dev-maker & cargo dev-taker -- & yarn --cwd=./maker-frontend dev  & yarn --cwd=./taker-frontend dev)
