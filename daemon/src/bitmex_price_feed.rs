cc01c16ea694ff054c3262ff801b156e2d7da767
Thomas Eizinger <thomas@eizinger.io>
2022-01-21 14:45:22 +1100

    Use `Tasks::add_fallible` API for price feed
    This results in us catching one more error: the failed parsing of the
    quote. Previously, we just terminated the task without also terminating
    the actor. Now we catch all these errors and terminate the actor
    afterwards. Hopefully this will make the price feed more resilient.
    
    May fix #1137.

--- daemon/src/bitmex_price_feed.rs
+++ daemon/src/bitmex_price_feed.rs
@@ -11,6 +11,7 @@ use std::convert::TryFrom;
 use std::time::Duration;
 use time::OffsetDateTime;
 use tokio_tungstenite::tungstenite;
+use xtra::Disconnected;
 use xtra_productivity::xtra_productivity;
 
 pub const QUOTE_INTERVAL_MINUTES: i64 = 1;
@@ -18,11 +19,11 @@ pub const QUOTE_INTERVAL_MINUTES: i64 = 1;
 pub struct Actor {
     tasks: Tasks,
     latest_quote: Option<Quote>,
-    supervisor: xtra::Address<supervisor::Actor<Self, StopReason>>,
+    supervisor: xtra::Address<supervisor::Actor<Self, Error>>,
 }
 
 impl Actor {
-    pub fn new(supervisor: xtra::Address<supervisor::Actor<Self, StopReason>>) -> Self {
+    pub fn new(supervisor: xtra::Address<supervisor::Actor<Self, Error>>) -> Self {
         Self {
             tasks: Tasks::default(),
             latest_quote: None,
@@ -34,19 +35,15 @@ impl Actor {
 #[async_trait]
 impl xtra::Actor for Actor {
     async fn started(&mut self, ctx: &mut xtra::Context<Self>) {
-        self.tasks.add({
-            let this = ctx.address().expect("we are alive");
+        let this = ctx.address().expect("we are alive");
+
+        self.tasks.add_fallible({
+            let this = this.clone();
 
             async move {
                 tracing::debug!("Connecting to BitMex realtime API");
 
-                let mut connection = match tokio_tungstenite::connect_async(format!("wss://www.bitmex.com/realtime?subscribe=quoteBin{QUOTE_INTERVAL_MINUTES}m:XBTUSD")).await {
-                    Ok((connection, _)) => connection,
-                    Err(e) => {
-                        let _ = this.send(StopReason::FailedToConnect { source: e }).await;
-                        return
-                    }
-                };
+                let (mut connection, _) = tokio_tungstenite::connect_async(format!("wss://www.bitmex.com/realtime?subscribe=quoteBin{QUOTE_INTERVAL_MINUTES}m:XBTUSD")).await.map_err(|e| Error::FailedToConnect { source: e })?;
 
                 tracing::info!("Connected to BitMex realtime API");
 
@@ -57,53 +54,49 @@ impl xtra::Actor for Actor {
                             let _ = connection.send(tungstenite::Message::Ping([0u8; 32].to_vec())).await;
                         },
                         msg = connection.try_next() => {
+                            let msg = msg.map_err(|e| Error::Failed { source: e })?;
+                            let msg = msg.ok_or(Error::StreamEnded)?;
+
                             match msg {
-                                Ok(Some(tungstenite::Message::Pong(_))) => {
+                                tungstenite::Message::Pong(_) => {
                                     tracing::trace!("Received pong");
                                     continue;
                                 }
-                                Ok(Some(tungstenite::Message::Text(text))) => {
-                                    match Quote::from_str(&text) {
-                                        Ok(None) => {
-                                            continue;
-                                        }
-                                        Ok(Some(quote)) => {
+                                tungstenite::Message::Text(text) => {
+                                    let quote = Quote::from_str(&text).map_err(|e| Error::FailedToParseQuote { source: e })?;
+
+                                    match quote {
+                                        Some(quote) => {
                                             let is_our_address_disconnected = this.send(NewQuoteReceived(quote)).await.is_err();
 
+                                            // Our task should already be dead and the actor restarted if this happens.
                                             if is_our_address_disconnected {
-                                                return;
+                                                return Ok(());
                                             }
                                         }
-                                        Err(e) => {
-                                            tracing::warn!("Failed to parse quote: {:#}", e);
-                                            return;
+                                        None => {
+                                            continue;
                                         }
                                     }
                                 }
-                                Ok(Some(other)) => {
+                                other => {
                                     tracing::trace!("Unsupported message: {:?}", other);
                                     continue;
-                                }
-                                Ok(None) => {
-                                    let _ = this.send(StopReason::StreamEnded).await;
-                                    return;
-                                }
-                                Err(e) => {
-                                    let _ = this.send(StopReason::Failed { source: e }).await;
-                                    return;
                                 }
                             }
                         },
                     }
                 }
             }
+        }, |e: Error| async move {
+            let _: Result<(), Disconnected> = this.send(e).await;
         });
     }
 }
 
 #[xtra_productivity]
 impl Actor {
-    async fn handle(&mut self, msg: StopReason, ctx: &mut xtra::Context<Self>) {
+    async fn handle(&mut self, msg: Error, ctx: &mut xtra::Context<Self>) {
         let _ = self
             .supervisor
             .send(supervisor::Stopped { reason: msg })
@@ -121,13 +114,15 @@ impl Actor {
 }
 
 #[derive(thiserror::Error, Debug)]
-pub enum StopReason {
+pub enum Error {
     #[error("Connection to BitMex API failed")]
     Failed { source: tungstenite::Error },
     #[error("Failed to connect to BitMex API")]
     FailedToConnect { source: tungstenite::Error },
     #[error("Websocket stream to BitMex API closed")]
     StreamEnded,
+    #[error("Failed to parse quote")]
+    FailedToParseQuote { source: anyhow::Error },
 }
 
 /// Private message to update our internal state with the latest quote.
