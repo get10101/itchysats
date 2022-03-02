e92f7fb66e2922dd82d9591bdba29858b7c949aa
Thomas Eizinger <thomas@eizinger.io>
2022-03-02 19:48:36 +1100

    Add prometheus counter for sent messages

--- daemon/src/maker_inc_connections.rs
+++ daemon/src/maker_inc_connections.rs
@@ -115,6 +115,10 @@ impl Connection {
         let msg_str = msg.name();
         let taker_id = self.taker;
 
+        P2P_MESSAGES_SENT
+            .with(&HashMap::from([(MESSAGE_LABEL, msg_str)]))
+            .inc();
+
         tracing::trace!(target: "wire", %taker_id, "Sending {msg_str}");
 
         self.write
@@ -545,6 +549,18 @@ static NUM_CONNECTIONS_GAUGE: conquer_once::Lazy<prometheus::IntGauge> =
         prometheus::register_int_gauge!(
             "p2p_connections_total",
             "The number of active p2p connections."
+        )
+        .unwrap()
+    });
+
+const MESSAGE_LABEL: &str = "message";
+
+static P2P_MESSAGES_SENT: conquer_once::Lazy<prometheus::IntCounterVec> =
+    conquer_once::Lazy::new(|| {
+        prometheus::register_int_counter_vec!(
+            "p2p_messages_sent_total",
+            "The number of messages sent over the p2p connection.",
+            &[MESSAGE_LABEL]
         )
         .unwrap()
     });
