9b6f2b63e143d23b681eb1f4c58786e24602b3a5
Thomas Eizinger <thomas@eizinger.io>
2022-03-09 13:28:38 +1100

    Make user-provided level the default log level
    Instead of logging stuff on `info` by default, we should respect
    the level the user is providing to us. To avoid this being too
    excessive even when they provide `Debug`, we set logs of several
    libraries to `warn`.

--- shared-bin/src/logger.rs
+++ shared-bin/src/logger.rs
@@ -14,16 +14,21 @@ pub fn init(level: LevelFilter, json_format: bool) -> Result<()> {
     let is_terminal = atty::is(atty::Stream::Stderr);
 
     let filter = EnvFilter::from_default_env()
-        .add_directive("info".parse()?)
+        .add_directive(format!("{level}").parse()?)
         .add_directive("sqlx=warn".parse()?) // sqlx logs all queries on INFO
+        .add_directive("hyper=warn".parse()?)
+        .add_directive("rustls=warn".parse()?)
+        .add_directive("reqwest=warn".parse()?)
+        .add_directive("tungstenite=warn".parse()?)
+        .add_directive("tokio_tungstenite=warn".parse()?)
+        .add_directive("electrum_client=warn".parse()?)
+        .add_directive("want=warn".parse()?)
+        .add_directive("mio=warn".parse()?)
+        .add_directive("tokio_util=warn".parse()?)
         .add_directive("bdk::blockchain::script_sync=off".parse()?) // bdk logs duration of sync on INFO
         .add_directive("bdk::wallet=off".parse()?) // bdk logs derivation of addresses on INFO
         .add_directive("_=off".parse()?) // rocket logs headers on INFO and uses `_` as the log target for it?
-        .add_directive("rocket=off".parse()?) // disable rocket logs: we have our own
-        .add_directive(format!("shared_bin={level}").parse()?)
-        .add_directive(format!("taker={level}").parse()?)
-        .add_directive(format!("maker={level}").parse()?)
-        .add_directive(format!("daemon={level}").parse()?);
+        .add_directive("rocket=off".parse()?); // disable rocket logs: we have our own
 
     let builder = tracing_subscriber::fmt()
         .with_env_filter(filter)
