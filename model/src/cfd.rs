0c192a8377cebd5c71452c34838f2c930b7c02ba
Thomas Eizinger <thomas@eizinger.io>
2022-04-29 14:00:12 +1000

    Don't add signatures to unsigned tx in failure case

--- model/src/cfd.rs
+++ model/src/cfd.rs
@@ -1030,7 +1030,7 @@ impl Cfd {
     }
 
     pub fn propose_collaborative_settlement(
-        &self,
+        &self, // TODO: This should not be a reference!
         current_price: Price,
         n_payouts: usize,
     ) -> Result<(CfdEvent, SettlementProposal, Transaction, Signature, Script)> {
