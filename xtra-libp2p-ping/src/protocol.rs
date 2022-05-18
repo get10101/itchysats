b043d3b43989927b944f0f51b01cf5b8c527de5f
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2022-05-18 12:10:29 +1000

    Set xtra_libp2p_ping::protocol functions as pub(crate)

--- xtra-libp2p-ping/src/protocol.rs
+++ xtra-libp2p-ping/src/protocol.rs
@@ -2,7 +2,7 @@
 //!
 //! Insbired by https://github.com/libp2p/rust-libp2p/blob/102509afe3a3b984e43a88dbe4de935fde36f319/protocols/ping/src/protocol.rs#L82-L113.
 
-pub const SIZE: usize = 32;
+pub(crate) const SIZE: usize = 32;
 
 use futures::AsyncReadExt;
 use futures::AsyncWriteExt;
@@ -15,7 +15,7 @@ use std::time::Instant;
 use xtra_libp2p::Substream;
 
 /// Sends a ping and waits for the pong.
-pub async fn send(mut stream: Substream) -> io::Result<Duration> {
+pub(crate) async fn send(mut stream: Substream) -> io::Result<Duration> {
     let payload: [u8; SIZE] = thread_rng().sample(distributions::Standard);
     stream.write_all(&payload).await?;
     stream.flush().await?;
@@ -36,7 +36,7 @@ pub async fn send(mut stream: Substream) -> io::Result<Duration> {
 }
 
 /// Waits for a ping and sends a pong.
-pub async fn recv(mut stream: Substream) -> io::Result<()> {
+pub(crate) async fn recv(mut stream: Substream) -> io::Result<()> {
     let mut payload = [0u8; SIZE];
     stream.read_exact(&mut payload).await?;
     stream.write_all(&payload).await?;
