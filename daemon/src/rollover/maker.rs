85e4819591ff21bba4ce6ffb7d95f1023ad9bab8
Daniel Karzel <daniel@comit.network>
2022-05-19 10:19:23 +1000

    WIP - Remove support for V1 from rollover over libp2p

--- daemon/src/rollover/maker.rs
+++ daemon/src/rollover/maker.rs
@@ -161,7 +161,6 @@ impl Actor {
                     Position::Short => short_funding_rate,
                 };
 
-                // TODO: Move the version into the actor if we want to be backwards compatible
                 let (event, params, dlc, position, interval) =
                     cfd.accept_rollover_proposal(tx_fee_rate, funding_rate, RolloverVersion::V2)?;
 
@@ -172,26 +171,10 @@ impl Actor {
         let oracle_event_id =
             oracle::next_announcement_after(time::OffsetDateTime::now_utc() + interval);
 
-        // the maker computes the rollover fee and sends it over to the taker so that both parties
-        // are on the same page
-        let complete_fee = match rollover_params.version {
-            RolloverVersion::V1 => {
-                // Note there is actually a bug here, but we have to keep this as is to reach
-                // agreement on the fee for the protocol V1 version.
-                //
-                // The current fee is supposed to be added here, but we never noticed because in V1
-                // the fee is always charged for one hour using a static rate. This
-                // results in applying the fee in the DLC only for the next rollover
-                // (because we do apply the fee in the Cfd when loading the rollover
-                // event). Effectively this means, that we always charged one
-                // rollover too little.
-                rollover_params.fee_account.settle()
-            }
-            RolloverVersion::V2 => rollover_params
-                .fee_account
-                .add_funding_fee(rollover_params.current_fee)
-                .settle(),
-        };
+        let complete_fee = rollover_params
+            .fee_account
+            .add_funding_fee(rollover_params.current_fee)
+            .settle();
 
         framed
             .send(ListenerMessage::Decision(Decision::Confirm(Confirm {
