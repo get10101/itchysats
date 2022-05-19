ec6e1083688865da3045be34c73bb2c1e5943f9d
Daniel Karzel <daniel@comit.network>
2022-05-19 14:07:30 +1000

    WIP - Optimize maker's rollover task after accept

--- daemon/src/rollover/maker.rs
+++ daemon/src/rollover/maker.rs
@@ -145,98 +145,104 @@ impl Actor {
             .remove(&order_id)
             .with_context(|| format!("No active protocol for order {order_id}"))?;
 
-        let (rollover_params, dlc, position, interval, funding_rate) = self
-            .executor
-            .execute(order_id, |cfd| {
-                let funding_rate = match cfd.position() {
-                    Position::Long => long_funding_rate,
-                    Position::Short => short_funding_rate,
-                };
+        self.tasks.add_fallible(
+            {
+                let executor = self.executor.clone();
+                let get_announcement = self.get_announcement.clone_channel();
+                let oracle_pk = self.oracle_pk;
+                let n_payouts = self.n_payouts;
+                async move {
+                    let (rollover_params, dlc, position, interval, funding_rate) = executor
+                        .execute(order_id, |cfd| {
+                            let funding_rate = match cfd.position() {
+                                Position::Long => long_funding_rate,
+                                Position::Short => short_funding_rate,
+                            };
 
-                let (event, params, dlc, position, interval) =
-                    cfd.accept_rollover_proposal(tx_fee_rate, funding_rate, RolloverVersion::V2)?;
+                            let (event, params, dlc, position, interval) = cfd
+                                .accept_rollover_proposal(
+                                    tx_fee_rate,
+                                    funding_rate,
+                                    RolloverVersion::V2,
+                                )?;
 
-                Ok((event, params, dlc, position, interval, funding_rate))
-            })
-            .await?;
+                            Ok((event, params, dlc, position, interval, funding_rate))
+                        })
+                        .await?;
 
-        let oracle_event_id =
-            oracle::next_announcement_after(time::OffsetDateTime::now_utc() + interval);
+                    let oracle_event_id =
+                        oracle::next_announcement_after(time::OffsetDateTime::now_utc() + interval);
 
-        let complete_fee = rollover_params
-            .fee_account
-            .add_funding_fee(rollover_params.current_fee)
-            .settle();
+                    let complete_fee = rollover_params
+                        .fee_account
+                        .add_funding_fee(rollover_params.current_fee)
+                        .settle();
 
-        // TODO: Consider spinning of task here and not only with roll_over - depends on what
-        // information we want to communicate back to the caller
+                    framed
+                        .send(ListenerMessage::Decision(Decision::Confirm(Confirm {
+                            order_id,
+                            oracle_event_id,
+                            tx_fee_rate,
+                            funding_rate,
+                            complete_fee: complete_fee.into(),
+                        })))
+                        .await
+                        .context("Failed to send Msg1::Accept")?;
 
-        framed
-            .send(ListenerMessage::Decision(Decision::Confirm(Confirm {
-                order_id,
-                oracle_event_id,
-                tx_fee_rate,
-                funding_rate,
-                complete_fee: complete_fee.into(),
-            })))
-            .await
-            .context("Failed to send Msg1::Accept")?;
+                    let announcement = get_announcement
+                        .send(oracle::GetAnnouncement(oracle_event_id))
+                        .await
+                        .context("Oracle actor disconnected")?
+                        .context("Failed to get announcement")?;
 
-        let announcement = self
-            .get_announcement
-            .send(oracle::GetAnnouncement(oracle_event_id))
-            .await
-            .context("Oracle actor disconnected")?
-            .context("Failed to get announcement")?;
+                    let funding_fee = *rollover_params.funding_fee();
 
-        let funding_fee = *rollover_params.funding_fee();
+                    let (sink, stream) = framed.split();
 
-        let (sink, stream) = framed.split();
+                    let dlc = setup_contract::roll_over(
+                        sink.with(|msg| future::ok(ListenerMessage::RolloverMsg(msg))),
+                        Box::pin(stream.filter_map(|msg| async move {
+                            if let Ok(msg) = msg {
+                                msg.into_rollover_msg().ok()
+                            } else {
+                                None
+                            }
+                        }))
+                        .fuse(),
+                        (oracle_pk, announcement),
+                        rollover_params,
+                        Role::Maker,
+                        position,
+                        dlc,
+                        n_payouts,
+                        complete_fee,
+                    )
+                    .await?;
 
-        let rollover_fut = setup_contract::roll_over(
-            sink.with(|msg| future::ok(ListenerMessage::RolloverMsg(msg))),
-            Box::pin(stream.filter_map(|msg| async move {
-                if let Ok(msg) = msg {
-                    msg.into_rollover_msg().ok()
-                } else {
-                    None
+                    if let Err(e) = executor
+                        .execute(order_id, |cfd| Ok(cfd.complete_rollover(dlc, funding_fee)))
+                        .await
+                    {
+                        tracing::warn!(%order_id, "Failed to execute rollover completed: {e:#}")
+                    }
+
+                    Ok(())
                 }
-            }))
-            .fuse(),
-            (self.oracle_pk, announcement),
-            rollover_params,
-            Role::Maker,
-            position,
-            dlc,
-            self.n_payouts,
-            complete_fee,
+            },
+            {
+                let executor = self.executor.clone();
+                move |e| async move {
+                    tracing::error!(%order_id, "Rollover failed: {e:#}");
+
+                    if let Err(e) = executor
+                        .execute(order_id, |cfd| Ok(cfd.fail_rollover(e)))
+                        .await
+                    {
+                        tracing::error!(%order_id, "Failed to execute rollover failed: {e:#}")
+                    }
+                }
+            },
         );
-
-        self.tasks.add({
-            let executor = self.executor.clone();
-            async move {
-                match rollover_fut.await {
-                    Ok(dlc) => {
-                        if let Err(e) = executor
-                            .execute(order_id, |cfd| Ok(cfd.complete_rollover(dlc, funding_fee)))
-                            .await
-                        {
-                            tracing::warn!(%order_id, "Failed to execute rollover completed: {e:#}")
-                        }
-                    }
-                    Err(e) => {
-                        tracing::warn!(%order_id, "Rollover protocol failed: {e:#}");
-
-                        if let Err(e) = executor
-                            .execute(order_id, |cfd| Ok(cfd.fail_rollover(e)))
-                            .await
-                        {
-                            tracing::error!(%order_id, "Failed to execute rollover failed: {e:#}")
-                        }
-                    }
-                }
-            }
-        });
 
         Ok(())
     }
