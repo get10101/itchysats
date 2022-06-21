db6da9753c28bc88966ff83372c9138917408fe3
Philipp Hoenisch <philipp@hoenisch.at>
2022-06-21 14:57:49 +1000

    Fix redirection bug
    All the useLatestEvent, useWebsocket, etc - hooks make the `App` load more than once.
    Because of this, the website is rendered repeatedly.
    Given the default url `/` had a redirect to `/long` we ended up redirection repeatedly until the browser stopped us. The result was, an empty page on the first load.
    To overcome this I added a manual logic which only runs on a page refresh, i.e. if you change the url or land on the side the first time it will be triggered.
    The result is a faster loading and less redirection.
    The downside is, that we will need to keep the logic up-2-date if we ever want to add a new route.

--- taker-frontend/src/main.tsx
+++ taker-frontend/src/main.tsx
@@ -1,17 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
 import theme from "./theme";
-import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<React.StrictMode>
-    <ChakraProvider theme={theme}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ChakraProvider>
-</React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ChakraProvider>
+    </React.StrictMode>,
+);
