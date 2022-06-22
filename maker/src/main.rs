fec048953226e6309b2123f2b79aa9c5894462f0
Mariusz Klochowicz <mariusz@klochowicz.com>
2022-06-22 09:10:54 +0200

    Log Rocket state after landing
    This makes it explicit that we're not silencing any errors etc.
    
    Log the values on trace, as it contains a lot of data about Rocket "mission" and
    therefore can be spammy.

--- maker/src/main.rs
+++ maker/src/main.rs
@@ -162,7 +162,7 @@ async fn main() -> Result<()> {
         projection::Actor::new(db.clone(), bitcoin_network, &price_feed);
     tasks.add(projection_context.run(proj_actor));
 
-    let _ = rocket::custom(figment)
+    let mission_success = rocket::custom(figment)
         .manage(projection_feeds)
         .manage(wallet_feed_receiver)
         .manage(maker)
@@ -190,6 +190,8 @@ async fn main() -> Result<()> {
         .attach(fairings::log_requests())
         .launch()
         .await?;
+
+    tracing::trace!(?mission_success, "Rocket has landed");
 
     db.close().await;
 
