f24111db576072078ea376fa0e2a7eef5d4f8414
Philipp Hoenisch <philipp@hoenisch.at>
2022-07-12 15:54:09 +1000

    Replace fetch with axios

--- taker-frontend/src/fetchVersion.ts
+++ taker-frontend/src/fetchVersion.ts
@@ -4,18 +4,29 @@ import { SemVer } from "semver";
 import { DaemonVersion, GithubVersion } from "./types";
 
 export async function fetchGithubVersion(setGithubVersion: (version: SemVer | null) => void) {
-    let response = await fetch("https://api.github.com/repos/itchysats/itchysats/releases/latest", {
-        headers: { accept: "application/json" },
-    });
+    try {
+        const response: AxiosResponse<GithubVersion> = await axios.get(
+            "https://api.github.com/repos/itchysats/itchysats/releases/latest",
+        );
 
-    let githubVersion = await response.json() as GithubVersion;
-    if (githubVersion?.tag_name) {
-        let version = semver.parse(githubVersion?.tag_name);
-        if (!version) {
-            // needed because 0.4.20.1 is not a valid semver version
-            version = semver.coerce(githubVersion?.tag_name);
+        let githubVersion = response.data;
+
+        if (githubVersion?.tag_name) {
+            let version = semver.parse(githubVersion?.tag_name);
+            if (!version) {
+                // needed because 0.4.20.1 is not a valid semver version
+                version = semver.coerce(githubVersion?.tag_name);
+            }
+            setGithubVersion(version);
         }
-        setGithubVersion(version);
+    } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            console.error(error);
+        } else {
+            // if it is not coming from axios then something else is odd and we just throw it up
+            throw error;
+        }
+        return;
     }
 }
 
--- taker-frontend/src/usePostRequest.ts
+++ taker-frontend/src/usePostRequest.ts
@@ -1,5 +1,6 @@
 import { useToast } from "@chakra-ui/react";
 import { useAsync } from "@react-hookz/web";
+import axios, { AxiosResponse } from "axios";
 
 /**
  * A React hook for sending a POST request to a certain endpoint.
@@ -14,42 +15,31 @@ export default function usePostRequest<Req = any, Res = any>(
 
     let [{ status }, { execute }] = useAsync(
         async ([payload]: any[]) => {
-            let res = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    "Content-type": "application/json",
-                },
-                credentials: "include",
-            });
-
-            if (!res.status.toString().startsWith("2")) {
-                let problem = await res.json() as Problem;
-
-                toast({
-                    title: "Error: " + problem.title,
-                    description: problem.detail,
-                    status: "error",
-                    duration: 10000,
-                    isClosable: true,
+            try {
+                let res: AxiosResponse<Res> = await axios.post(url, payload, {
+                    headers: {
+                        "Content-type": "application/json",
+                    },
+                    withCredentials: true,
                 });
+
+                onSuccess(res.data);
+            } catch (error: any) {
+                if (axios.isAxiosError(error)) {
+                    let problem = error.toJSON() as Problem;
+                    toast({
+                        title: "Error: " + problem.title,
+                        description: problem.detail,
+                        status: "error",
+                        duration: 10000,
+                        isClosable: true,
+                    });
+                } else {
+                    // if it is not coming from axios then something else is odd and we just throw it up
+                    throw error;
+                }
                 return;
             }
-
-            let responseType = res.headers.get("Content-type");
-
-            if (responseType && responseType.startsWith("application/json")) {
-                onSuccess(await res.json() as Res);
-                return;
-            }
-
-            if (responseType && responseType.startsWith("text/plain")) {
-                onSuccess(await res.text() as unknown as Res); // `unknown` cast is not ideal because we known that `.text()` gives us string.
-                return;
-            }
-
-            // if none of the above content types match, pass bytes to the caller
-            onSuccess(await res.blob() as unknown as Res); // `unknown` cast is not ideal because we known that `.blob()` gives us as blob.
         },
     );
     return [execute, status === "loading"];
