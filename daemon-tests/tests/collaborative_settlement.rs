84c15909c2ee25de8a388c632cd35eb900b6c358
Daniel Karzel <daniel@comit.network>
2022-07-22 07:44:30 +0200

    Restructure test files to put tests on top
    Tests on top, supporting functions at the bottom.

--- daemon-tests/tests/collaborative_settlement.rs
+++ daemon-tests/tests/collaborative_settlement.rs
@@ -21,34 +21,6 @@ async fn collaboratively_close_an_open_cfd_maker_going_long() {
     collaboratively_close_an_open_cfd(Position::Long).await;
 }
 
-async fn collaboratively_close_an_open_cfd(maker_position: Position) {
-    let (mut maker, mut taker, order_id, _) =
-        start_from_open_cfd_state(OliviaData::example_0().announcement(), maker_position).await;
-    taker.mocks.mock_latest_quote(Some(dummy_quote())).await;
-    maker.mocks.mock_latest_quote(Some(dummy_quote())).await;
-    next_with(taker.quote_feed(), |q| q).await.unwrap(); // if quote is available on feed, it propagated through the system
-
-    taker.system.propose_settlement(order_id).await.unwrap();
-
-    wait_next_state!(
-        order_id,
-        maker,
-        taker,
-        CfdState::IncomingSettlementProposal,
-        CfdState::OutgoingSettlementProposal
-    );
-
-    maker.system.accept_settlement(order_id).await.unwrap();
-    sleep(Duration::from_secs(5)).await; // need to wait a bit until both transition
-
-    wait_next_state!(order_id, maker, taker, CfdState::PendingClose);
-    confirm!(close transaction, order_id, maker, taker);
-
-    sleep(Duration::from_secs(5)).await; // need to wait a bit until both transition
-
-    wait_next_state!(order_id, maker, taker, CfdState::Closed);
-}
-
 #[otel_test]
 async fn maker_rejects_collab_settlement_after_commit_finality() {
     let (mut maker, mut taker, order_id, _) =
@@ -99,4 +71,32 @@ async fn maker_accepts_collab_settlement_after_commit_finality() {
     sleep(Duration::from_secs(5)).await; // need to wait a bit until both transition
 
     wait_next_state!(order_id, maker, taker, CfdState::OpenCommitted);
+}
+
+async fn collaboratively_close_an_open_cfd(maker_position: Position) {
+    let (mut maker, mut taker, order_id, _) =
+        start_from_open_cfd_state(OliviaData::example_0().announcement(), maker_position).await;
+    taker.mocks.mock_latest_quote(Some(dummy_quote())).await;
+    maker.mocks.mock_latest_quote(Some(dummy_quote())).await;
+    next_with(taker.quote_feed(), |q| q).await.unwrap(); // if quote is available on feed, it propagated through the system
+
+    taker.system.propose_settlement(order_id).await.unwrap();
+
+    wait_next_state!(
+        order_id,
+        maker,
+        taker,
+        CfdState::IncomingSettlementProposal,
+        CfdState::OutgoingSettlementProposal
+    );
+
+    maker.system.accept_settlement(order_id).await.unwrap();
+    sleep(Duration::from_secs(5)).await; // need to wait a bit until both transition
+
+    wait_next_state!(order_id, maker, taker, CfdState::PendingClose);
+    confirm!(close transaction, order_id, maker, taker);
+
+    sleep(Duration::from_secs(5)).await; // need to wait a bit until both transition
+
+    wait_next_state!(order_id, maker, taker, CfdState::Closed);
 }
