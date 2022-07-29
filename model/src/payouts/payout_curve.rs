a25d1edc120505112dc05587c14e026f8fdeb8fc
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2022-07-29 18:31:22 +1000

    Create payout_curve::prop_compose test submodule
    So that the `proptest::Strategy`s can be reused in other modules.

--- model/src/payouts/payout_curve.rs
+++ model/src/payouts/payout_curve.rs
@@ -485,6 +485,10 @@ fn create_long_payout_function(
 
 #[cfg(test)]
 mod tests {
+    use super::prop_compose::arb_contracts;
+    use super::prop_compose::arb_fee_flow;
+    use super::prop_compose::arb_leverage;
+    use super::prop_compose::arb_price;
     use super::*;
     use bdk::bitcoin::Amount;
     use proptest::prelude::*;
@@ -880,28 +884,52 @@ mod tests {
         }
     }
 
+    fn payout(range: RangeInclusive<u64>, short: u64, long: u64) -> PayoutParameter {
+        PayoutParameter {
+            left_bound: *range.start(),
+            right_bound: *range.end(),
+            long_amount: long,
+            short_amount: short,
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod prop_compose {
+    use crate::CompleteFee;
+    use crate::Leverage;
+    use crate::Price;
+    use crate::Usd;
+    use bdk::bitcoin::Amount;
+    use num::FromPrimitive;
+    use proptest::prop_compose;
+    use rust_decimal::Decimal;
+
     prop_compose! {
-        fn arb_price(min: f64, max: f64)(price in min..max) -> Price {
+        pub fn arb_price(min: f64, max: f64)(price in min..max) -> Price {
             let price = Decimal::from_f64(price).unwrap();
 
             Price::new(price).unwrap()
         }
     }
 
+    #[cfg(test)]
     prop_compose! {
-        fn arb_contracts(min: u64, max: u64)(contracts in min..max) -> Usd {
+        pub fn arb_contracts(min: u64, max: u64)(contracts in min..max) -> Usd {
             let contracts = Decimal::from(contracts);
 
             Usd::new(contracts)
         }
     }
 
+    #[cfg(test)]
     prop_compose! {
-        fn arb_leverage(min: u8, max: u8)(leverage in min..max) -> Leverage {
+        pub fn arb_leverage(min: u8, max: u8)(leverage in min..max) -> Leverage {
             Leverage::new(leverage).unwrap()
         }
     }
 
+    #[cfg(test)]
     prop_compose! {
         /// Generate an arbitrary fee flow value, between the `lower`
         /// and `upper` bounds.
@@ -909,7 +937,7 @@ mod tests {
         /// A positive value represents a fee flow from long to short.
         /// Conversely, a negative valure represents a fee flow from
         /// short to long.
-        fn arb_fee_flow(lower: i64, upper: i64)(fee in lower..upper) -> CompleteFee {
+        pub fn arb_fee_flow(lower: i64, upper: i64)(fee in lower..upper) -> CompleteFee {
             let fee_amount = Amount::from_sat(fee.abs().try_into().unwrap());
             if fee.is_positive() {
                 CompleteFee::LongPaysShort(fee_amount)
@@ -918,15 +946,6 @@ mod tests {
             } else {
                 CompleteFee::None
             }
-        }
-    }
-
-    fn payout(range: RangeInclusive<u64>, short: u64, long: u64) -> PayoutParameter {
-        PayoutParameter {
-            left_bound: *range.start(),
-            right_bound: *range.end(),
-            long_amount: long,
-            short_amount: short,
         }
     }
 }
