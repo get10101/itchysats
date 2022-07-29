4d482a8a0a1a9896c00b392f1ae1e72fbc87aa10
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2022-07-29 14:54:22 +1000

    Improve resilience of connectivity actor test
    Explicit arbitrary sleeps are often either inefficient or unreliable.
    It's generally better to replace them with loops that check a
    condition, if possible.

--- daemon-tests/tests/connectivity.rs
+++ daemon-tests/tests/connectivity.rs
@@ -1,12 +1,10 @@
 use daemon::online_status::ConnectionStatus;
-use daemon_tests::flow::next;
+use daemon_tests::flow::next_with;
 use daemon_tests::Maker;
 use daemon_tests::MakerConfig;
 use daemon_tests::Taker;
 use daemon_tests::TakerConfig;
 use otel_tests::otel_test;
-use std::time::Duration;
-use tokio_extras::time::sleep;
 
 #[otel_test]
 async fn taker_notices_lack_of_maker() {
@@ -24,30 +22,27 @@ async fn taker_notices_lack_of_maker() {
     )
     .await;
 
-    sleep(Duration::from_secs(5)).await; // wait a bit until taker notices change
-
-    assert_eq!(
-        ConnectionStatus::Online,
-        next(taker.maker_status_feed()).await.unwrap()
-    );
+    wait_next_connection_status_to_maker(&mut taker, ConnectionStatus::Online).await;
 
     drop(maker);
 
-    sleep(Duration::from_secs(5)).await; // wait a bit until taker notices change
-
-    assert_eq!(
-        ConnectionStatus::Offline,
-        next(taker.maker_status_feed()).await.unwrap(),
-    );
+    wait_next_connection_status_to_maker(&mut taker, ConnectionStatus::Offline).await;
 
     let _maker = Maker::start(&maker_config).await;
 
-    sleep(Duration::from_secs(5)).await; // wait a bit until taker notices change
+    wait_next_connection_status_to_maker(&mut taker, ConnectionStatus::Online).await;
+}
 
-    assert_eq!(
-        ConnectionStatus::Online,
-        next(taker.maker_status_feed()).await.unwrap(),
-    );
+/// Wait indefinitely until the `taker`'s connection status to the maker is the one `expected` by
+/// the caller.
+async fn wait_next_connection_status_to_maker(taker: &mut Taker, expected: ConnectionStatus) {
+    let is_expected = next_with(taker.maker_status_feed(), |actual| {
+        (actual == expected).then_some(())
+    })
+    .await
+    .is_ok();
+
+    assert!(is_expected)
 }
 
 // TODO: Reinstate maker_notices_lack_of_taker by allowing to query Endpoint
