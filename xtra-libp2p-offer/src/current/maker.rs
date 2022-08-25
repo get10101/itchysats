get10101/itchysats

commit e4f910f35070c47672c8e37e224b51e3b532e833

feat(network): Add /itchysats/offer/2.0.0
The `/itchysats/offer/2.0.0` protocol offers proper support for
sending and receiving offers with different contract symbols. The
design of the message exchanged between maker and taker has changed to
make this easier. These are the key differences:

- Before, we only allowed the maker to send a pair of offers that were
supposed to reference the same contract symbol, which was sufficient
for BTCUSD. Now, we give freedom to the maker to send a list of
offers. The taker must combine the new offers with the old ones to
derive a full view of the latest offers.

- Before, parts of the message were optional to allow the maker to
"disable" certain offers and even rollovers. This feature is not
needed since both the maker has other mechanisms in place to reject
orders and rollovers. Now, all fields are mandatory (sending an empty
list is technically possible, but it doesn't mean anything for the
taker).

The maker supports versions 1.0.0 and 2.0.0 side by side. We maintain
version 1.0.0 for the maker in a `deprecated` module, but remove a lot
of the code that is no longer necessary (the taker side and the
tests). We have adapted what remains so that it is easier to integrate
it in `maker` together with the new protocol.

Because the maker is the dialer in the offer protocol, it means that
currently all their takers will get doubled offers (one per version)
and will ignore the ones that correspond to the protocol they don't
support. To prevent us from generating false positive warnings on the
maker side, we filter out the `yamux::NegotiationError::Failed` error.

Additionally:

- Rework the way the offers are modelled in different components
across the entire codebase.
- The `projection::Actor` now takes a `Role` parameter, so that we can
do away with the awkward `Origin` type in the new offer protocol.
- Simplify `model::Offer`, which is not actually used anywhere
where backwards-compatibility matters.

xtra-libp2p-offer/src/current/maker.rs
@@ -1,6 +1,5 @@
-use crate::protocol;
-use crate::MakerOffers;
-use crate::PROTOCOL;
+use crate::current::protocol;
+use crate::current::PROTOCOL;
 use async_trait::async_trait;
 use std::collections::HashSet;
 use std::time::Duration;
@@ -16,7 +15,7 @@ use xtra_productivity::xtra_productivity;
 pub struct Actor {
     endpoint: xtra::Address<Endpoint>,
     connected_peers: HashSet<PeerId>,
-    latest_offers: Option<MakerOffers>,
+    latest_offers: Vec<model::Offer>,
 }
 
 impl Actor {
@@ -24,7 +23,7 @@ impl Actor {
         Self {
             endpoint,
             connected_peers: HashSet::default(),
-            latest_offers: None,
+            latest_offers: Vec::new(),
         }
     }
 
@@ -39,13 +38,22 @@ impl Actor {
                 .await??
                 .await?;
 
-            protocol::send(stream, offers).await?;
+            protocol::send(stream, offers.into()).await?;
 
             anyhow::Ok(())
         };
 
-        let err_handler =
-            move |e| async move { tracing::warn!(%peer_id, "Failed to send offers: {e:#}") };
+        let err_handler = move |e: anyhow::Error| async move {
+            if let Some(xtra_libp2p::Error::NegotiationFailed(
+                xtra_libp2p::NegotiationError::Failed,
+            )) = e.downcast_ref::<xtra_libp2p::Error>()
+            {
+                // It's normal to disagree on the protocols now that we broadcast on both versions
+                // to _all_ our peers
+            } else {
+                tracing::warn!(%peer_id, "Failed to send offers: {e:#}")
+            }
+        };
 
         let this = ctx.address().expect("self to be alive");
         spawn_fallible(&this, task.instrument(span), err_handler);
@@ -67,8 +75,8 @@ impl Actor {
         }
     }
 
-    async fn handle(&mut self, _: GetLatestOffers) -> Option<model::MakerOffers> {
-        self.latest_offers.clone().map(|offer| offer.into())
+    async fn handle(&mut self, _: GetLatestOffers) -> Vec<model::Offer> {
+        self.latest_offers.clone()
     }
 }
 
@@ -92,10 +100,10 @@ impl Actor {
 
 /// Instruct the `offer::maker::Actor` to broadcast to all
 /// connected peers an update to the current offers.
-pub struct NewOffers(pub Option<MakerOffers>);
+pub struct NewOffers(Vec<model::Offer>);
 
 impl NewOffers {
-    pub fn new(offers: Option<MakerOffers>) -> Self {
+    pub fn new(offers: Vec<model::Offer>) -> Self {
         Self(offers)
     }
 }

xtra-libp2p-offer/src/lib.rs
@@ -1,172 +1,21 @@
-use model::olivia::BitMexPriceEventId;
-use model::ContractSymbol;
-use model::FundingRate;
-use model::Leverage;
-use model::OfferId;
-use model::OpeningFee;
-use model::Origin;
-use model::Position;
-use model::Price;
-use model::Timestamp;
-use model::TxFeeRate;
-use model::Usd;
-use serde::Deserialize;
-use serde::Serialize;
-use std::fmt;
-use time::Duration;
+mod current;
+pub mod deprecated;
 
-pub mod maker;
-mod protocol;
-pub mod taker;
-
-pub const PROTOCOL: &str = "/itchysats/offer/1.0.0";
-
-#[derive(Clone, Serialize, Deserialize, PartialEq)]
-pub struct MakerOffers {
-    pub long: Option<Offer>,
-    pub short: Option<Offer>,
-    pub tx_fee_rate: TxFeeRate,
-    pub funding_rate_long: FundingRate,
-    pub funding_rate_short: FundingRate,
-}
-
-impl fmt::Debug for MakerOffers {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt.debug_struct("MakerOffers")
-            .field("long_order_id", &self.long.as_ref().map(|o| o.id))
-            .field("short_order_id", &self.short.as_ref().map(|o| o.id))
-            .field("tx_fee_rate", &self.tx_fee_rate)
-            .field("funding_rate_long", &self.funding_rate_long)
-            .field("funding_rate_short", &self.funding_rate_short)
-            .finish()
-    }
-}
-
-/// A concrete order created by a maker for a taker
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct Offer {
-    pub id: OfferId,
-
-    #[serde(rename = "trading_pair")]
-    pub contract_symbol: ContractSymbol,
-
-    /// The maker's position
-    ///
-    /// Since the maker is the creator of this order this always reflects the maker's position.
-    /// When we create a Cfd we change it to be the position as seen by each party, i.e. flip the
-    /// position for the taker.
-    #[serde(rename = "position")]
-    pub position_maker: Position,
-
-    pub price: Price,
-
-    pub min_quantity: Usd,
-    pub max_quantity: Usd,
-
-    /// A selection of leverages that the maker allows for the taker
-    pub leverage_choices: Vec<Leverage>,
-
-    /// The creation timestamp as set by the maker
-    #[serde(rename = "creation_timestamp")]
-    pub creation_timestamp_maker: Timestamp,
-
-    /// The duration that will be used for calculating the settlement timestamp
-    pub settlement_interval: Duration,
-
-    pub origin: Origin,
-
-    /// The id of the event to be used for price attestation
-    ///
-    /// The maker includes this into the Order based on the Oracle announcement to be used.
-    pub oracle_event_id: BitMexPriceEventId,
-
-    pub tx_fee_rate: TxFeeRate,
-    pub funding_rate: FundingRate,
-    pub opening_fee: OpeningFee,
-}
-
-impl From<MakerOffers> for model::MakerOffers {
-    fn from(maker_offers: MakerOffers) -> Self {
-        model::MakerOffers {
-            long: maker_offers.long.map(|offer| offer.into()),
-            short: maker_offers.short.map(|offer| offer.into()),
-            tx_fee_rate: maker_offers.tx_fee_rate,
-            funding_rate_long: maker_offers.funding_rate_long,
-            funding_rate_short: maker_offers.funding_rate_short,
-        }
-    }
-}
-
-#[allow(deprecated)] // allowing for backwards compatible conversion
-impl From<Offer> for model::Offer {
-    fn from(offer: Offer) -> Self {
-        model::Offer {
-            id: offer.id,
-            contract_symbol: offer.contract_symbol,
-            position_maker: offer.position_maker,
-            price: offer.price,
-            min_quantity: offer.min_quantity,
-            max_quantity: offer.max_quantity,
-            leverage_taker: Leverage::TWO,
-            leverage_choices: offer.leverage_choices,
-            creation_timestamp_maker: offer.creation_timestamp_maker,
-            settlement_interval: offer.settlement_interval,
-            origin: offer.origin,
-            oracle_event_id: offer.oracle_event_id,
-            tx_fee_rate: offer.tx_fee_rate,
-            funding_rate: offer.funding_rate,
-            opening_fee: offer.opening_fee,
-        }
-    }
-}
-
-impl From<model::MakerOffers> for MakerOffers {
-    fn from(maker_offers: model::MakerOffers) -> Self {
-        MakerOffers {
-            long: maker_offers.long.map(|offer| offer.into()),
-            short: maker_offers.short.map(|offer| offer.into()),
-            tx_fee_rate: maker_offers.tx_fee_rate,
-            funding_rate_long: maker_offers.funding_rate_long,
-            funding_rate_short: maker_offers.funding_rate_short,
-        }
-    }
-}
-
-impl From<model::Offer> for Offer {
-    fn from(offer: model::Offer) -> Self {
-        Offer {
-            id: offer.id,
-            contract_symbol: offer.contract_symbol,
-            position_maker: offer.position_maker,
-            price: offer.price,
-            min_quantity: offer.min_quantity,
-            max_quantity: offer.max_quantity,
-            leverage_choices: offer.leverage_choices,
-            creation_timestamp_maker: offer.creation_timestamp_maker,
-            settlement_interval: offer.settlement_interval,
-            origin: offer.origin,
-            oracle_event_id: offer.oracle_event_id,
-            tx_fee_rate: offer.tx_fee_rate,
-            funding_rate: offer.funding_rate,
-            opening_fee: offer.opening_fee,
-        }
-    }
-}
+pub use current::*;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::taker::LatestMakerOffers;
+    use crate::taker::LatestOffers;
     use async_trait::async_trait;
     use futures::Future;
-    use futures::FutureExt;
     use model::olivia::BitMexPriceEventId;
     use model::ContractSymbol;
     use model::FundingRate;
     use model::Leverage;
-    use model::Origin;
     use model::Position;
     use model::Price;
+    use model::Timestamp;
     use model::TxFeeRate;
     use model::Usd;
     use rust_decimal::Decimal;
@@ -213,7 +62,7 @@ mod tests {
             .unwrap()
             .unwrap();
 
-        let new_offers = dummy_maker_offers();
+        let new_offers = dummy_offers();
 
         // maker keeps sending the offers until the taker establishes
         // a connection
@@ -236,16 +85,11 @@ mod tests {
         // get the maker's latest offers
         let received_offers = retry_until_some(|| {
             let offer_receiver_addr = offer_receiver_addr.clone();
-            async move {
-                offer_receiver_addr
-                    .send(GetLatestOffers)
-                    .map(|res| res.unwrap())
-                    .await
-            }
+            async move { offer_receiver_addr.send(GetLatestOffers).await.unwrap() }
         })
         .await;
 
-        assert_eq!(new_offers, Some(received_offers))
+        assert_eq!(new_offers, received_offers)
     }
 
     fn create_endpoint_with_offer_maker(
@@ -297,14 +141,12 @@ mod tests {
     }
 
     struct OffersReceiver {
-        latest_offers: Option<MakerOffers>,
+        offers: Vec<model::Offer>,
     }
 
     impl OffersReceiver {
         fn new() -> Self {
-            Self {
-                latest_offers: None,
-            }
+            Self { offers: Vec::new() }
         }
     }
 
@@ -317,8 +159,8 @@ mod tests {
 
     #[xtra_productivity]
     impl OffersReceiver {
-        async fn handle(&mut self, msg: LatestMakerOffers) {
-            self.latest_offers = msg.0.map(|offer| offer.into());
+        async fn handle(&mut self, msg: LatestOffers) {
+            self.offers = msg.0;
         }
     }
 
@@ -326,51 +168,47 @@ mod tests {
 
     #[xtra_productivity]
     impl OffersReceiver {
-        async fn handle(&mut self, _: GetLatestOffers) -> Option<MakerOffers> {
-            self.latest_offers.clone()
+        async fn handle(&mut self, _: GetLatestOffers) -> Vec<model::Offer> {
+            self.offers.clone()
         }
     }
 
-    async fn retry_until_some<F, FUT, T>(mut fut: F) -> T
+    async fn retry_until_some<F, FUT>(mut fut: F) -> Vec<model::Offer>
     where
         F: FnMut() -> FUT,
-        FUT: Future<Output = Option<T>>,
+        FUT: Future<Output = Vec<model::Offer>>,
     {
         loop {
-            match fut().await {
-                Some(t) => return t,
-                None => tokio_extras::time::sleep(Duration::from_millis(200)).await,
+            let offers = fut().await;
+
+            if offers.is_empty() {
+                tokio_extras::time::sleep(Duration::from_millis(200)).await;
+            } else {
+                return offers;
             }
         }
     }
 
-    pub fn dummy_maker_offers() -> Option<MakerOffers> {
-        Some(MakerOffers {
-            long: Some(dummy_offer(Position::Long)),
-            short: Some(dummy_offer(Position::Short)),
-            tx_fee_rate: TxFeeRate::default(),
-            funding_rate_long: FundingRate::new(Decimal::ONE).unwrap(),
-            funding_rate_short: FundingRate::new(Decimal::NEGATIVE_ONE).unwrap(),
-        })
+    pub fn dummy_offers() -> Vec<model::Offer> {
+        vec![dummy_offer(Position::Long), dummy_offer(Position::Short)]
     }
 
-    fn dummy_offer(position: Position) -> Offer {
-        Offer {
+    fn dummy_offer(position_maker: Position) -> model::Offer {
+        model::Offer {
             id: Default::default(),
             contract_symbol: ContractSymbol::BtcUsd,
-            position_maker: position,
+            position_maker,
             price: Price::new(dec!(1000)).unwrap(),
             min_quantity: Usd::new(dec!(100)),
             max_quantity: Usd::new(dec!(1000)),
             leverage_choices: vec![Leverage::TWO],
             creation_timestamp_maker: Timestamp::now(),
             settlement_interval: time::Duration::hours(24),
-            origin: Origin::Ours,
             oracle_event_id: BitMexPriceEventId::with_20_digits(
                 datetime!(2021-10-04 22:00:00).assume_utc(),
             ),
-            tx_fee_rate: Default::default(),
-            funding_rate: Default::default(),
+            tx_fee_rate: TxFeeRate::default(),
+            funding_rate: FundingRate::new(Decimal::ONE).unwrap(),
             opening_fee: Default::default(),
         }
     }

daemon-tests/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::flow::is_next_offers_none;
+use crate::flow::ensure_null_next_offers;
 use crate::flow::next_maker_offers;
 use crate::flow::next_with;
 use crate::flow::one_cfd_with_state;
@@ -280,6 +280,7 @@ impl Default for OpenCfdArgs {
 /// This allows callers to use it as a starting point for their test.
 pub async fn open_cfd(taker: &mut Taker, maker: &mut Maker, args: OpenCfdArgs) -> OrderId {
     let offer_params = args.offer_params();
+    let contract_symbol = offer_params.contract_symbol;
     let OpenCfdArgs {
         oracle_data,
         position_maker,
@@ -288,7 +289,7 @@ pub async fn open_cfd(taker: &mut Taker, maker: &mut Maker, args: OpenCfdArgs) -
         ..
     } = args;
 
-    is_next_offers_none(taker.offers_feed()).await.unwrap();
+    ensure_null_next_offers(taker.offers_feed()).await.unwrap();
 
     tracing::debug!("Sending {offer_params:?}");
     maker.set_offer_params(offer_params).await;
@@ -305,9 +306,11 @@ pub async fn open_cfd(taker: &mut Taker, maker: &mut Maker, args: OpenCfdArgs) -
 
     mock_oracle_announcements(maker, taker, oracle_data.announcements()).await;
 
-    let offer_to_take = match position_maker {
-        Position::Short => received.short,
-        Position::Long => received.long,
+    let offer_to_take = match (contract_symbol, position_maker) {
+        (ContractSymbol::BtcUsd, Position::Long) => received.btcusd_long,
+        (ContractSymbol::BtcUsd, Position::Short) => received.btcusd_short,
+        (ContractSymbol::EthUsd, Position::Long) => received.ethusd_long,
+        (ContractSymbol::EthUsd, Position::Short) => received.ethusd_short,
     }
     .context("Order for expected position not set")
     .unwrap();
@@ -692,7 +695,7 @@ impl Maker {
         );
 
         let (proj_actor, feeds) =
-            projection::Actor::new(db, Network::Testnet, price_feed_addr.into());
+            projection::Actor::new(db, Network::Testnet, price_feed_addr.into(), Role::Maker);
         tasks.add(projection_context.run(proj_actor));
 
         Self {
@@ -889,6 +892,7 @@ impl Taker {
             db.clone(),
             Network::Testnet,
             taker.price_feed_actor.clone().into(),
+            Role::Taker,
         );
         tasks.add(projection_context.run(proj_actor));
 

daemon/src/lib.rs
@@ -20,7 +20,6 @@ use model::libp2p::PeerId;
 use model::olivia;
 use model::Identity;
 use model::Leverage;
-use model::Offer;
 use model::OfferId;
 use model::OrderId;
 use model::Price;

daemon/src/order/maker.rs
@@ -25,8 +25,6 @@ use maia_core::PartyParams;
 use model::olivia;
 use model::Cfd;
 use model::Identity;
-use model::MakerOffers;
-use model::Offer;
 use model::OfferId;
 use model::OrderId;
 use model::Role;
@@ -54,7 +52,7 @@ pub struct Actor {
     n_payouts: usize,
     decision_senders: HashMap<OrderId, oneshot::Sender<protocol::Decision>>,
     db: sqlite_db::Connection,
-    latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Option<MakerOffers>>,
+    latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Vec<model::Offer>>,
 }
 
 impl Actor {
@@ -71,10 +69,7 @@ impl Actor {
             MessageChannel<wallet::Sign, Result<PartiallySignedTransaction>>,
         ),
         projection: xtra::Address<projection::Actor>,
-        latest_offers: MessageChannel<
-            xtra_libp2p_offer::maker::GetLatestOffers,
-            Option<MakerOffers>,
-        >,
+        latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Vec<model::Offer>>,
     ) -> Self {
         Self {
             executor: command::Executor::new(db.clone(), process_manager),
@@ -107,17 +102,18 @@ impl Actor {
     }
 
     #[instrument(skip(self), err)]
-    async fn pick_offer(&self, offer_id: OfferId) -> Result<Offer> {
+    async fn pick_offer(&self, offer_id: OfferId) -> Result<model::Offer> {
         let latest_offers = self
             .latest_offers
             .send(xtra_libp2p_offer::maker::GetLatestOffers)
             .await
-            .context("Failed to retrieve latest offer from offers actor")?
-            .context("No latest offers available")?;
+            .context("Failed to retrieve latest offer from offers actor")?;
 
         let offer = latest_offers
-            .pick_offer_to_take(offer_id)
-            .with_context(|| format!("Offer with id {offer_id} not found in current offers"))?;
+            .iter()
+            .find(|offer| offer.id == offer_id)
+            .with_context(|| format!("Offer with id {offer_id} not found in current offers"))?
+            .clone();
 
         Ok(offer)
     }

model/src/lib.rs
@@ -56,9 +56,9 @@ pub const SETTLEMENT_INTERVAL: time::Duration = time::Duration::hours(24);
 
 #[derive(thiserror::Error, Debug, Clone, Copy)]
 pub enum Error {
-    #[error("Price of zero is not allowed.")]
+    #[error("Price of zero is not allowed")]
     ZeroPrice,
-    #[error("Negative Price is unimplemented.")]
+    #[error("Negative Price is unimplemented")]
     NegativePrice,
 }
 
@@ -462,7 +462,7 @@ pub enum ContractSymbol {
     EthUsd,
 }
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub enum Position {
     Long,
     Short,

xtra-libp2p-offer/src/deprecated/maker.rs
@@ -1,6 +1,6 @@
-use crate::protocol;
-use crate::MakerOffers;
-use crate::PROTOCOL;
+use crate::deprecated;
+use crate::deprecated::protocol;
+use crate::deprecated::protocol::MakerOffers;
 use async_trait::async_trait;
 use std::collections::HashSet;
 use std::time::Duration;
@@ -35,7 +35,10 @@ impl Actor {
         let span = tracing::debug_span!("Send offers", %peer_id).or_current();
         let task = async move {
             let stream = endpoint
-                .send(OpenSubstream::single_protocol(peer_id, PROTOCOL))
+                .send(OpenSubstream::single_protocol(
+                    peer_id,
+                    deprecated::PROTOCOL,
+                ))
                 .await??
                 .await?;
 
@@ -44,8 +47,17 @@ impl Actor {
             anyhow::Ok(())
         };
 
-        let err_handler =
-            move |e| async move { tracing::warn!(%peer_id, "Failed to send offers: {e:#}") };
+        let err_handler = move |e: anyhow::Error| async move {
+            if let Some(xtra_libp2p::Error::NegotiationFailed(
+                xtra_libp2p::NegotiationError::Failed,
+            )) = e.downcast_ref::<xtra_libp2p::Error>()
+            {
+                // It's normal to disagree on the protocols now that we broadcast on both versions
+                // to _all_ our peers
+            } else {
+                tracing::warn!(%peer_id, "Failed to send offers: {e:#}")
+            }
+        };
 
         let this = ctx.address().expect("self to be alive");
         spawn_fallible(&this, task.instrument(span), err_handler);
@@ -55,7 +67,7 @@ impl Actor {
 #[xtra_productivity]
 impl Actor {
     async fn handle(&mut self, msg: NewOffers, ctx: &mut xtra::Context<Self>) {
-        self.latest_offers = msg.0;
+        self.latest_offers = MakerOffers::new(msg.0);
 
         let quiet = quiet_spans::sometimes_quiet_children();
         for peer_id in self.connected_peers.iter().copied() {
@@ -65,10 +77,6 @@ impl Actor {
                 }))
                 .await
         }
-    }
-
-    async fn handle(&mut self, _: GetLatestOffers) -> Option<model::MakerOffers> {
-        self.latest_offers.clone().map(|offer| offer.into())
     }
 }
 
@@ -92,16 +100,13 @@ impl Actor {
 
 /// Instruct the `offer::maker::Actor` to broadcast to all
 /// connected peers an update to the current offers.
-pub struct NewOffers(pub Option<MakerOffers>);
+pub struct NewOffers(Vec<model::Offer>);
 
 impl NewOffers {
-    pub fn new(offers: Option<MakerOffers>) -> Self {
+    pub fn new(offers: Vec<model::Offer>) -> Self {
         Self(offers)
     }
 }
-
-#[derive(Clone, Copy)]
-pub struct GetLatestOffers;
 
 #[async_trait]
 impl xtra::Actor for Actor {

commit 8528cbb84e839076bb78e768607e0e2e221d0712

fix: Ensure maker shares all offers with newly connected taker
We introduced a regression with
e4f910f35070c47672c8e37e224b51e3b532e833 and this patch solves it.

We add a test which failed before introducing the changes.

xtra-libp2p-offer/src/current/maker.rs
@@ -1,6 +1,9 @@
 use crate::current::protocol;
 use crate::current::PROTOCOL;
 use async_trait::async_trait;
+use model::ContractSymbol;
+use model::Position;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::time::Duration;
 use tokio_extras::spawn_fallible;
@@ -15,7 +18,7 @@ use xtra_productivity::xtra_productivity;
 pub struct Actor {
     endpoint: xtra::Address<Endpoint>,
     connected_peers: HashSet<PeerId>,
-    latest_offers: Vec<model::Offer>,
+    current_offers: Offers,
 }
 
 impl Actor {
@@ -23,13 +26,17 @@ impl Actor {
         Self {
             endpoint,
             connected_peers: HashSet::default(),
-            latest_offers: Vec::new(),
+            current_offers: Offers::default(),
         }
     }
 
-    async fn send_offers(&self, peer_id: PeerId, ctx: &mut xtra::Context<Self>) {
+    async fn send_offers(
+        &self,
+        peer_id: PeerId,
+        offers: Vec<model::Offer>,
+        ctx: &mut xtra::Context<Self>,
+    ) {
         let endpoint = self.endpoint.clone();
-        let offers = self.latest_offers.clone();
 
         let span = tracing::debug_span!("Send offers", %peer_id).or_current();
         let task = async move {
@@ -63,11 +70,11 @@ impl Actor {
 #[xtra_productivity]
 impl Actor {
     async fn handle(&mut self, msg: NewOffers, ctx: &mut xtra::Context<Self>) {
-        self.latest_offers = msg.0;
+        self.current_offers.update(msg.0.clone());
 
         let quiet = quiet_spans::sometimes_quiet_children();
         for peer_id in self.connected_peers.iter().copied() {
-            self.send_offers(peer_id, ctx)
+            self.send_offers(peer_id, msg.0.clone(), ctx)
                 .instrument(quiet.in_scope(|| {
                     tracing::debug_span!("Broadcast offers to taker (libp2p)").or_current()
                 }))
@@ -76,7 +83,7 @@ impl Actor {
     }
 
     async fn handle(&mut self, _: GetLatestOffers) -> Vec<model::Offer> {
-        self.latest_offers.clone()
+        self.current_offers.to_vec()
     }
 }
 
@@ -89,7 +96,8 @@ impl Actor {
     ) {
         tracing::trace!("Adding newly established connection: {:?}", msg.peer_id);
         self.connected_peers.insert(msg.peer_id);
-        self.send_offers(msg.peer_id, ctx).await;
+        self.send_offers(msg.peer_id, self.current_offers.to_vec(), ctx)
+            .await;
     }
 
     async fn handle_connection_dropped(&mut self, msg: endpoint::ConnectionDropped) {
@@ -110,6 +118,22 @@ impl NewOffers {
 
 #[derive(Clone, Copy)]
 pub struct GetLatestOffers;
+
+#[derive(Clone, Default)]
+struct Offers(HashMap<(ContractSymbol, Position), model::Offer>);
+
+impl Offers {
+    fn update(&mut self, offers: Vec<model::Offer>) {
+        for offer in offers.into_iter() {
+            self.0
+                .insert((offer.contract_symbol, offer.position_maker), offer);
+        }
+    }
+
+    fn to_vec(&self) -> Vec<model::Offer> {
+        self.0.iter().map(|(_, offer)| offer).cloned().collect()
+    }
+}
 
 #[async_trait]
 impl xtra::Actor for Actor {

xtra-libp2p-offer/src/lib.rs
@@ -93,6 +93,61 @@ mod tests {
         assert_eq!(new_offers, received_offers)
     }
 
+    #[tokio::test]
+    async fn given_taker_connects_then_taker_receives_all_current_offers() {
+        tracing_subscriber::fmt()
+            .with_env_filter("xtra_libp2p_offer=trace")
+            .with_test_writer()
+            .init();
+
+        let (maker_peer_id, maker_offer_addr, maker_endpoint_addr) =
+            create_endpoint_with_offer_maker();
+        let (offer_receiver_addr, taker_endpoint_addr) = create_endpoint_with_offer_taker();
+
+        maker_endpoint_addr
+            .send(ListenOn(Multiaddr::empty().with(Protocol::Memory(1000))))
+            .await
+            .unwrap();
+
+        let offer_btc_usd_long = dummy_offer(ContractSymbol::BtcUsd, Position::Long);
+        maker_offer_addr
+            .send(crate::maker::NewOffers::new(vec![
+                offer_btc_usd_long.clone()
+            ]))
+            .await
+            .unwrap();
+
+        let offer_eth_usd_short = dummy_offer(ContractSymbol::EthUsd, Position::Short);
+        maker_offer_addr
+            .send(crate::maker::NewOffers::new(vec![
+                offer_eth_usd_short.clone()
+            ]))
+            .await
+            .unwrap();
+
+        taker_endpoint_addr
+            .send(Connect(
+                Multiaddr::empty()
+                    .with(Protocol::Memory(1000))
+                    .with(Protocol::P2p(maker_peer_id.into())),
+            ))
+            .await
+            .unwrap()
+            .unwrap();
+
+        // taker retries until the connection is established and we
+        // get the maker's latest offers
+        let received_offers = retry_until_some(|| {
+            let offer_receiver_addr = offer_receiver_addr.clone();
+            async move { offer_receiver_addr.send(GetLatestOffers).await.unwrap() }
+        })
+        .await;
+
+        assert_eq!(received_offers.len(), 2);
+        assert!(received_offers.contains(&offer_btc_usd_long));
+        assert!(received_offers.contains(&offer_eth_usd_short));
+    }
+
     fn create_endpoint_with_offer_maker(
     ) -> (PeerId, Address<crate::maker::Actor>, Address<Endpoint>) {
         let (endpoint_addr, endpoint_context) = Context::new(None);
@@ -191,13 +246,16 @@ mod tests {
     }
 
     pub fn dummy_offers() -> Vec<model::Offer> {
-        vec![dummy_offer(Position::Long), dummy_offer(Position::Short)]
+        vec![
+            dummy_offer(ContractSymbol::BtcUsd, Position::Long),
+            dummy_offer(ContractSymbol::BtcUsd, Position::Short),
+        ]
     }
 
-    fn dummy_offer(position_maker: Position) -> model::Offer {
+    fn dummy_offer(contract_symbol: ContractSymbol, position_maker: Position) -> model::Offer {
         model::Offer {
             id: Default::default(),
-            contract_symbol: ContractSymbol::BtcUsd,
+            contract_symbol,
             position_maker,
             price: Price::new(dec!(1000)).unwrap(),
             min_quantity: Contracts::new(100),