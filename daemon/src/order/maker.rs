get10101/itchysats

commit e4f910f35070c47672c8e37e224b51e3b532e833

feat(network): Add /itchysats/offer/2.0.0
The `/itchysats/offer/2.0.0` protocol offers proper support for
sending and receiving offers with different contract symbols. The
design of the message exchanged between maker and taker has changed to
make this easier. These are the key differences:

- Before, we only allowed the maker to send a pair of offers that were
supposed to reference the same contract symbol, which was sufficient
for BTCUSD. Now, we give freedom to the maker to send a list of
offers. The taker must combine the new offers with the old ones to
derive a full view of the latest offers.

- Before, parts of the message were optional to allow the maker to
"disable" certain offers and even rollovers. This feature is not
needed since both the maker has other mechanisms in place to reject
orders and rollovers. Now, all fields are mandatory (sending an empty
list is technically possible, but it doesn't mean anything for the
taker).

The maker supports versions 1.0.0 and 2.0.0 side by side. We maintain
version 1.0.0 for the maker in a `deprecated` module, but remove a lot
of the code that is no longer necessary (the taker side and the
tests). We have adapted what remains so that it is easier to integrate
it in `maker` together with the new protocol.

Because the maker is the dialer in the offer protocol, it means that
currently all their takers will get doubled offers (one per version)
and will ignore the ones that correspond to the protocol they don't
support. To prevent us from generating false positive warnings on the
maker side, we filter out the `yamux::NegotiationError::Failed` error.

Additionally:

- Rework the way the offers are modelled in different components
across the entire codebase.
- The `projection::Actor` now takes a `Role` parameter, so that we can
do away with the awkward `Origin` type in the new offer protocol.
- Simplify `model::Offer`, which is not actually used anywhere
where backwards-compatibility matters.

daemon/src/order/maker.rs
@@ -25,8 +25,6 @@ use maia_core::PartyParams;
 use model::olivia;
 use model::Cfd;
 use model::Identity;
-use model::MakerOffers;
-use model::Offer;
 use model::OfferId;
 use model::OrderId;
 use model::Role;
@@ -54,7 +52,7 @@ pub struct Actor {
     n_payouts: usize,
     decision_senders: HashMap<OrderId, oneshot::Sender<protocol::Decision>>,
     db: sqlite_db::Connection,
-    latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Option<MakerOffers>>,
+    latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Vec<model::Offer>>,
 }
 
 impl Actor {
@@ -71,10 +69,7 @@ impl Actor {
             MessageChannel<wallet::Sign, Result<PartiallySignedTransaction>>,
         ),
         projection: xtra::Address<projection::Actor>,
-        latest_offers: MessageChannel<
-            xtra_libp2p_offer::maker::GetLatestOffers,
-            Option<MakerOffers>,
-        >,
+        latest_offers: MessageChannel<xtra_libp2p_offer::maker::GetLatestOffers, Vec<model::Offer>>,
     ) -> Self {
         Self {
             executor: command::Executor::new(db.clone(), process_manager),
@@ -107,17 +102,18 @@ impl Actor {
     }
 
     #[instrument(skip(self), err)]
-    async fn pick_offer(&self, offer_id: OfferId) -> Result<Offer> {
+    async fn pick_offer(&self, offer_id: OfferId) -> Result<model::Offer> {
         let latest_offers = self
             .latest_offers
             .send(xtra_libp2p_offer::maker::GetLatestOffers)
             .await
-            .context("Failed to retrieve latest offer from offers actor")?
-            .context("No latest offers available")?;
+            .context("Failed to retrieve latest offer from offers actor")?;
 
         let offer = latest_offers
-            .pick_offer_to_take(offer_id)
-            .with_context(|| format!("Offer with id {offer_id} not found in current offers"))?;
+            .iter()
+            .find(|offer| offer.id == offer_id)
+            .with_context(|| format!("Offer with id {offer_id} not found in current offers"))?
+            .clone();
 
         Ok(offer)
     }

xtra-libp2p-offer/src/current/maker.rs
@@ -1,6 +1,5 @@
-use crate::protocol;
-use crate::MakerOffers;
-use crate::PROTOCOL;
+use crate::current::protocol;
+use crate::current::PROTOCOL;
 use async_trait::async_trait;
 use std::collections::HashSet;
 use std::time::Duration;
@@ -16,7 +15,7 @@ use xtra_productivity::xtra_productivity;
 pub struct Actor {
     endpoint: xtra::Address<Endpoint>,
     connected_peers: HashSet<PeerId>,
-    latest_offers: Option<MakerOffers>,
+    latest_offers: Vec<model::Offer>,
 }
 
 impl Actor {
@@ -24,7 +23,7 @@ impl Actor {
         Self {
             endpoint,
             connected_peers: HashSet::default(),
-            latest_offers: None,
+            latest_offers: Vec::new(),
         }
     }
 
@@ -39,13 +38,22 @@ impl Actor {
                 .await??
                 .await?;
 
-            protocol::send(stream, offers).await?;
+            protocol::send(stream, offers.into()).await?;
 
             anyhow::Ok(())
         };
 
-        let err_handler =
-            move |e| async move { tracing::warn!(%peer_id, "Failed to send offers: {e:#}") };
+        let err_handler = move |e: anyhow::Error| async move {
+            if let Some(xtra_libp2p::Error::NegotiationFailed(
+                xtra_libp2p::NegotiationError::Failed,
+            )) = e.downcast_ref::<xtra_libp2p::Error>()
+            {
+                // It's normal to disagree on the protocols now that we broadcast on both versions
+                // to _all_ our peers
+            } else {
+                tracing::warn!(%peer_id, "Failed to send offers: {e:#}")
+            }
+        };
 
         let this = ctx.address().expect("self to be alive");
         spawn_fallible(&this, task.instrument(span), err_handler);
@@ -67,8 +75,8 @@ impl Actor {
         }
     }
 
-    async fn handle(&mut self, _: GetLatestOffers) -> Option<model::MakerOffers> {
-        self.latest_offers.clone().map(|offer| offer.into())
+    async fn handle(&mut self, _: GetLatestOffers) -> Vec<model::Offer> {
+        self.latest_offers.clone()
     }
 }
 
@@ -92,10 +100,10 @@ impl Actor {
 
 /// Instruct the `offer::maker::Actor` to broadcast to all
 /// connected peers an update to the current offers.
-pub struct NewOffers(pub Option<MakerOffers>);
+pub struct NewOffers(Vec<model::Offer>);
 
 impl NewOffers {
-    pub fn new(offers: Option<MakerOffers>) -> Self {
+    pub fn new(offers: Vec<model::Offer>) -> Self {
         Self(offers)
     }
 }

xtra-libp2p-offer/src/current/protocol.rs
@@ -1,0 +1,174 @@
+use asynchronous_codec::FramedRead;
+use asynchronous_codec::FramedWrite;
+use asynchronous_codec::JsonCodec;
+use asynchronous_codec::JsonCodecError;
+use futures::AsyncReadExt;
+use futures::AsyncWriteExt;
+use futures::SinkExt;
+use futures::StreamExt;
+use model::olivia::BitMexPriceEventId;
+use model::ContractSymbol;
+use model::FundingRate;
+use model::Leverage;
+use model::OfferId;
+use model::OpeningFee;
+use model::Position;
+use model::Price;
+use model::Timestamp;
+use model::TxFeeRate;
+use model::Usd;
+use serde::Deserialize;
+use serde::Serialize;
+use std::fmt;
+use time::Duration;
+
+pub(crate) async fn send<S>(sink: S, offers: Offers) -> Result<(), JsonCodecError>
+where
+    S: AsyncWriteExt + Unpin,
+{
+    let mut framed = FramedWrite::new(sink, JsonCodec::<Offers, ()>::new());
+    framed.send(offers).await?;
+    MESSAGES_SENT.inc();
+
+    Ok(())
+}
+
+pub(crate) async fn recv<S>(stream: S) -> Result<Offers, ReceiveError>
+where
+    S: AsyncReadExt + Unpin,
+{
+    let mut framed = FramedRead::new(stream, JsonCodec::<(), Offers>::new());
+
+    let offers = framed.next().await.ok_or(ReceiveError::Terminated)??;
+
+    MESSAGES_RECEIVED.inc();
+
+    Ok(offers)
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
+pub(crate) struct Offers(Vec<Offer>);
+
+#[derive(Clone, Serialize, Deserialize, PartialEq)]
+pub(crate) struct Offer {
+    id: OfferId,
+    contract_symbol: ContractSymbol,
+    position_maker: Position,
+    price: Price,
+    min_quantity: Usd,
+    max_quantity: Usd,
+    leverage_choices: Vec<Leverage>,
+    creation_timestamp_maker: Timestamp,
+    settlement_interval: Duration,
+    oracle_event_id: BitMexPriceEventId,
+    tx_fee_rate: TxFeeRate,
+    funding_rate: FundingRate,
+    opening_fee: OpeningFee,
+}
+
+impl From<model::Offer> for Offer {
+    fn from(offer: model::Offer) -> Self {
+        Self {
+            id: offer.id,
+            contract_symbol: offer.contract_symbol,
+            position_maker: offer.position_maker,
+            price: offer.price,
+            min_quantity: offer.min_quantity,
+            max_quantity: offer.max_quantity,
+            leverage_choices: offer.leverage_choices,
+            creation_timestamp_maker: offer.creation_timestamp_maker,
+            settlement_interval: offer.settlement_interval,
+            oracle_event_id: offer.oracle_event_id,
+            tx_fee_rate: offer.tx_fee_rate,
+            funding_rate: offer.funding_rate,
+            opening_fee: offer.opening_fee,
+        }
+    }
+}
+
+impl From<Offer> for model::Offer {
+    fn from(offer: Offer) -> Self {
+        Self {
+            id: offer.id,
+            contract_symbol: offer.contract_symbol,
+            position_maker: offer.position_maker,
+            price: offer.price,
+            min_quantity: offer.min_quantity,
+            max_quantity: offer.max_quantity,
+            leverage_choices: offer.leverage_choices,
+            creation_timestamp_maker: offer.creation_timestamp_maker,
+            settlement_interval: offer.settlement_interval,
+            oracle_event_id: offer.oracle_event_id,
+            tx_fee_rate: offer.tx_fee_rate,
+            funding_rate: offer.funding_rate,
+            opening_fee: offer.opening_fee,
+        }
+    }
+}
+
+impl From<Vec<model::Offer>> for Offers {
+    fn from(offers: Vec<model::Offer>) -> Self {
+        Offers(offers.into_iter().map(Offer::from).collect())
+    }
+}
+
+impl From<Offers> for Vec<model::Offer> {
+    fn from(offers: Offers) -> Self {
+        offers.0.into_iter().map(model::Offer::from).collect()
+    }
+}
+
+impl fmt::Debug for Offer {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("Offer")
+            .field("contract_symbol", &self.contract_symbol)
+            .field("position_maker", &self.position_maker)
+            .field("offer_id", &self.id)
+            .finish()
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum ReceiveError {
+    #[error("The stream has terminated")]
+    Terminated,
+    #[error("Failed to decode maker offers")]
+    Decode(#[from] JsonCodecError),
+}
+
+static MESSAGES_SENT: conquer_once::Lazy<prometheus::IntCounter> = conquer_once::Lazy::new(|| {
+    prometheus::register_int_counter!(
+        "offer_messages_sent_total_v2",
+        "The number of offer messages sent over the libp2p connection.",
+    )
+    .unwrap()
+});
+
+static MESSAGES_RECEIVED: conquer_once::Lazy<prometheus::IntCounter> =
+    conquer_once::Lazy::new(|| {
+        prometheus::register_int_counter!(
+            "offer_messages_received_total",
+            "The number of offer messages received over the libp2p connection.",
+        )
+        .unwrap()
+    });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::dummy_offers;
+    use sluice::pipe::pipe;
+
+    #[tokio::test]
+    async fn sent_offers_match_received_offers() {
+        let (stream, sink) = pipe();
+
+        let maker_offers = dummy_offers();
+
+        let (send_res, recv_res) =
+            tokio::join!(send(sink, Offers::from(maker_offers.clone())), recv(stream));
+
+        assert!(send_res.is_ok());
+        assert_eq!(maker_offers, Vec::<model::Offer>::from(recv_res.unwrap()))
+    }
+}

xtra-libp2p-offer/src/current/taker.rs
@@ -1,4 +1,4 @@
-use crate::protocol;
+use crate::current::protocol;
 use async_trait::async_trait;
 use tracing::Instrument;
 use xtra::prelude::MessageChannel;
@@ -6,11 +6,11 @@ use xtra_libp2p::NewInboundSubstream;
 use xtra_productivity::xtra_productivity;
 
 pub struct Actor {
-    maker_offers: MessageChannel<LatestMakerOffers, ()>,
+    maker_offers: MessageChannel<LatestOffers, ()>,
 }
 
 impl Actor {
-    pub fn new(maker_offers: MessageChannel<LatestMakerOffers, ()>) -> Self {
+    pub fn new(maker_offers: MessageChannel<LatestOffers, ()>) -> Self {
         Self { maker_offers }
     }
 }
@@ -25,9 +25,12 @@ impl Actor {
 
         let task = async move {
             let offers = protocol::recv(stream).await?;
-            let span = tracing::debug_span!("Received new offers from maker", %peer_id, ?offers);
+
+            tracing::debug!(?offers, "Received offers");
+
+            let span = tracing::debug_span!("Received new offers from maker", %peer_id);
             maker_offers
-                .send(LatestMakerOffers(offers.map(model::MakerOffers::from)))
+                .send(LatestOffers(offers.into()))
                 .instrument(span)
                 .await?;
 
@@ -44,7 +47,7 @@ impl Actor {
 
 /// Message used to inform other actors about the maker's latest
 /// offers.
-pub struct LatestMakerOffers(pub Option<model::MakerOffers>);
+pub struct LatestOffers(pub Vec<model::Offer>);
 
 #[async_trait]
 impl xtra::Actor for Actor {

xtra-libp2p-offer/src/deprecated/maker.rs
@@ -1,6 +1,6 @@
-use crate::protocol;
-use crate::MakerOffers;
-use crate::PROTOCOL;
+use crate::deprecated;
+use crate::deprecated::protocol;
+use crate::deprecated::protocol::MakerOffers;
 use async_trait::async_trait;
 use std::collections::HashSet;
 use std::time::Duration;
@@ -35,7 +35,10 @@ impl Actor {
         let span = tracing::debug_span!("Send offers", %peer_id).or_current();
         let task = async move {
             let stream = endpoint
-                .send(OpenSubstream::single_protocol(peer_id, PROTOCOL))
+                .send(OpenSubstream::single_protocol(
+                    peer_id,
+                    deprecated::PROTOCOL,
+                ))
                 .await??
                 .await?;
 
@@ -44,8 +47,17 @@ impl Actor {
             anyhow::Ok(())
         };
 
-        let err_handler =
-            move |e| async move { tracing::warn!(%peer_id, "Failed to send offers: {e:#}") };
+        let err_handler = move |e: anyhow::Error| async move {
+            if let Some(xtra_libp2p::Error::NegotiationFailed(
+                xtra_libp2p::NegotiationError::Failed,
+            )) = e.downcast_ref::<xtra_libp2p::Error>()
+            {
+                // It's normal to disagree on the protocols now that we broadcast on both versions
+                // to _all_ our peers
+            } else {
+                tracing::warn!(%peer_id, "Failed to send offers: {e:#}")
+            }
+        };
 
         let this = ctx.address().expect("self to be alive");
         spawn_fallible(&this, task.instrument(span), err_handler);
@@ -55,7 +67,7 @@ impl Actor {
 #[xtra_productivity]
 impl Actor {
     async fn handle(&mut self, msg: NewOffers, ctx: &mut xtra::Context<Self>) {
-        self.latest_offers = msg.0;
+        self.latest_offers = MakerOffers::new(msg.0);
 
         let quiet = quiet_spans::sometimes_quiet_children();
         for peer_id in self.connected_peers.iter().copied() {
@@ -65,10 +77,6 @@ impl Actor {
                 }))
                 .await
         }
-    }
-
-    async fn handle(&mut self, _: GetLatestOffers) -> Option<model::MakerOffers> {
-        self.latest_offers.clone().map(|offer| offer.into())
     }
 }
 
@@ -92,16 +100,13 @@ impl Actor {
 
 /// Instruct the `offer::maker::Actor` to broadcast to all
 /// connected peers an update to the current offers.
-pub struct NewOffers(pub Option<MakerOffers>);
+pub struct NewOffers(Vec<model::Offer>);
 
 impl NewOffers {
-    pub fn new(offers: Option<MakerOffers>) -> Self {
+    pub fn new(offers: Vec<model::Offer>) -> Self {
         Self(offers)
     }
 }
-
-#[derive(Clone, Copy)]
-pub struct GetLatestOffers;
 
 #[async_trait]
 impl xtra::Actor for Actor {

xtra-libp2p-offer/src/deprecated/protocol.rs
@@ -1,0 +1,124 @@
+use asynchronous_codec::FramedWrite;
+use asynchronous_codec::JsonCodec;
+use asynchronous_codec::JsonCodecError;
+use futures::AsyncWriteExt;
+use futures::SinkExt;
+use model::olivia::BitMexPriceEventId;
+use model::ContractSymbol;
+use model::FundingRate;
+use model::Leverage;
+use model::OfferId;
+use model::OpeningFee;
+use model::Origin;
+use model::Position;
+use model::Price;
+use model::Timestamp;
+use model::TxFeeRate;
+use model::Usd;
+use serde::Deserialize;
+use serde::Serialize;
+use time::Duration;
+
+pub(crate) async fn send<S>(sink: S, offers: Option<MakerOffers>) -> Result<(), JsonCodecError>
+where
+    S: AsyncWriteExt + Unpin,
+{
+    let mut framed = FramedWrite::new(sink, JsonCodec::<Option<MakerOffers>, ()>::new());
+    framed.send(offers).await?;
+    MESSAGES_SENT.inc();
+
+    Ok(())
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq)]
+pub(crate) struct MakerOffers {
+    long: Option<Offer>,
+    short: Option<Offer>,
+    tx_fee_rate: TxFeeRate,
+    funding_rate_long: FundingRate,
+    funding_rate_short: FundingRate,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub(crate) struct Offer {
+    id: OfferId,
+    #[serde(rename = "trading_pair")]
+    contract_symbol: ContractSymbol,
+    #[serde(rename = "position")]
+    position_maker: Position,
+    price: Price,
+    min_quantity: Usd,
+    max_quantity: Usd,
+    #[serde(rename = "leverage")]
+    leverage_taker: Leverage,
+    leverage_choices: Vec<Leverage>,
+    #[serde(rename = "creation_timestamp")]
+    creation_timestamp_maker: Timestamp,
+    settlement_interval: Duration,
+    origin: Origin,
+    oracle_event_id: BitMexPriceEventId,
+    tx_fee_rate: TxFeeRate,
+    funding_rate: FundingRate,
+    opening_fee: OpeningFee,
+}
+
+impl From<model::Offer> for Offer {
+    fn from(offer: model::Offer) -> Self {
+        Self {
+            id: offer.id,
+            contract_symbol: offer.contract_symbol,
+            position_maker: offer.position_maker,
+            price: offer.price,
+            min_quantity: offer.min_quantity,
+            max_quantity: offer.max_quantity,
+            leverage_taker: Leverage::TWO,
+            leverage_choices: offer.leverage_choices,
+            creation_timestamp_maker: offer.creation_timestamp_maker,
+            settlement_interval: offer.settlement_interval,
+            origin: Origin::Ours, // the maker always creates it
+            oracle_event_id: offer.oracle_event_id,
+            tx_fee_rate: offer.tx_fee_rate,
+            funding_rate: offer.funding_rate,
+            opening_fee: offer.opening_fee,
+        }
+    }
+}
+
+impl MakerOffers {
+    pub(crate) fn new(offers: Vec<model::Offer>) -> Option<Self> {
+        // We can safely assume that the new offers all have the same `tx_fee_rate`, because this
+        // field is redundant across offers
+        let tx_fee_rate = offers.first()?.tx_fee_rate;
+
+        // This version of the protocol caters to takers that only support BTCUSD CFDs
+        let mut offers = offers
+            .iter()
+            .filter(|offer| offer.contract_symbol == ContractSymbol::BtcUsd);
+
+        let long = offers.find_map(|offer| {
+            (offer.position_maker == Position::Long).then(|| Offer::from(offer.clone()))
+        })?;
+        let short = offers.find_map(|offer| {
+            (offer.position_maker == Position::Short).then(|| Offer::from(offer.clone()))
+        })?;
+
+        let funding_rate_long = long.funding_rate;
+        let funding_rate_short = short.funding_rate;
+
+        Some(Self {
+            long: Some(long),
+            short: Some(short),
+            tx_fee_rate,
+            funding_rate_long,
+            funding_rate_short,
+        })
+    }
+}
+
+static MESSAGES_SENT: conquer_once::Lazy<prometheus::IntCounter> = conquer_once::Lazy::new(|| {
+    prometheus::register_int_counter!(
+        "offer_messages_sent_total_v1",
+        "The number of offer messages sent over the libp2p connection.",
+    )
+    .unwrap()
+});

xtra-libp2p-offer/src/protocol.rs
@@ -1,88 +1,0 @@
-use crate::MakerOffers;
-use asynchronous_codec::FramedRead;
-use asynchronous_codec::FramedWrite;
-use asynchronous_codec::JsonCodec;
-use asynchronous_codec::JsonCodecError;
-use futures::AsyncReadExt;
-use futures::AsyncWriteExt;
-use futures::SinkExt;
-use futures::StreamExt;
-
-static MESSAGES_SENT: conquer_once::Lazy<prometheus::IntCounter> = conquer_once::Lazy::new(|| {
-    prometheus::register_int_counter!(
-        "offer_messages_sent_total",
-        "The number of offer messages sent over the libp2p connection.",
-    )
-    .unwrap()
-});
-
-static MESSAGES_RECEIVED: conquer_once::Lazy<prometheus::IntCounter> =
-    conquer_once::Lazy::new(|| {
-        prometheus::register_int_counter!(
-            "offer_messages_received_total",
-            "The number of offer messages received over the libp2p connection.",
-        )
-        .unwrap()
-    });
-
-pub(crate) async fn send<S>(sink: S, offers: Option<MakerOffers>) -> Result<(), JsonCodecError>
-where
-    S: AsyncWriteExt + Unpin,
-{
-    let mut framed = FramedWrite::new(sink, JsonCodec::<Option<MakerOffers>, ()>::new());
-    framed.send(offers).await?;
-    MESSAGES_SENT.inc();
-
-    Ok(())
-}
-
-pub(crate) async fn recv<S>(stream: S) -> Result<Option<MakerOffers>, ReceiveError>
-where
-    S: AsyncReadExt + Unpin,
-{
-    let mut framed = FramedRead::new(stream, JsonCodec::<(), Option<MakerOffers>>::new());
-
-    let offers = framed.next().await.ok_or(ReceiveError::Terminated)??;
-    MESSAGES_RECEIVED.inc();
-
-    Ok(offers)
-}
-
-#[derive(Debug, thiserror::Error)]
-pub(crate) enum ReceiveError {
-    #[error("The stream has terminated.")]
-    Terminated,
-    #[error("Failed to decode maker offers.")]
-    Decode(#[from] JsonCodecError),
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::tests::dummy_maker_offers;
-    use sluice::pipe::pipe;
-
-    #[tokio::test]
-    async fn can_execute_protocol_with_updated_offers() {
-        let (stream, sink) = pipe();
-
-        let maker_offers = dummy_maker_offers();
-
-        let (send_res, recv_res) = tokio::join!(send(sink, maker_offers.clone()), recv(stream));
-
-        assert!(send_res.is_ok());
-        assert_eq!(recv_res.unwrap(), maker_offers)
-    }
-
-    #[tokio::test]
-    async fn can_execute_protocol_with_none_offers() {
-        let (stream, sink) = pipe();
-
-        let maker_offers = None;
-
-        let (send_res, recv_res) = tokio::join!(send(sink, maker_offers.clone()), recv(stream));
-
-        assert!(send_res.is_ok());
-        assert_eq!(recv_res.unwrap(), maker_offers)
-    }
-}

daemon-tests/src/flow.rs
@@ -1,3 +1,4 @@
+use anyhow::ensure;
 use anyhow::Context;
 use anyhow::Result;
 use daemon::projection::Cfd;
@@ -22,13 +23,21 @@ pub async fn next_maker_offers(
 
     let non_empty_offer = |offers: MakerOffers| match &offers {
         MakerOffers {
-            short: Some(short),
-            long: Some(long),
-        } if &short.contract_symbol == contract_symbol
-            && &long.contract_symbol == contract_symbol =>
-        {
-            Some(offers)
-        }
+            btcusd_long: Some(_),
+            ..
+        } if contract_symbol == &ContractSymbol::BtcUsd => Some(offers),
+        MakerOffers {
+            btcusd_long: Some(_),
+            ..
+        } if contract_symbol == &ContractSymbol::BtcUsd => Some(offers),
+        MakerOffers {
+            ethusd_long: Some(_),
+            ..
+        } if contract_symbol == &ContractSymbol::EthUsd => Some(offers),
+        MakerOffers {
+            ethusd_long: Some(_),
+            ..
+        } if contract_symbol == &ContractSymbol::EthUsd => Some(offers),
         _ => None,
     };
 
@@ -40,9 +49,17 @@ pub async fn next_maker_offers(
     Ok((a?, b?))
 }
 
-pub async fn is_next_offers_none(rx: &mut watch::Receiver<MakerOffers>) -> Result<bool> {
+pub async fn ensure_null_next_offers(rx: &mut watch::Receiver<MakerOffers>) -> Result<()> {
     let maker_offers = next(rx).await?;
-    Ok(maker_offers.long.is_none() && maker_offers.short.is_none())
+
+    ensure!(
+        maker_offers.btcusd_long.is_none()
+            && maker_offers.btcusd_short.is_none()
+            && maker_offers.ethusd_long.is_none()
+            && maker_offers.ethusd_short.is_none()
+    );
+
+    Ok(())
 }
 
 /// Returns watch channel value upon change

commit 60ccaf24ee0f7516de03110b96c60e059dde5f2f

fix(offer): Restore compatibility with taker:0.5.5
This fixes an unintended breaking change introduced by
e4f910f35070c47672c8e37e224b51e3b532e833, which modified the
`model::Offer` type in several ways. At the time we assumed that we
were no longer sending said type over the wire, but that was wrong.

daemon/src/order/maker.rs
@@ -134,13 +134,13 @@ impl Actor {
             }
         };
 
-        let (order_id, offer, quantity, leverage) = match order {
+        let (order_id, offer_id, quantity, leverage) = match order {
             TakerMessage::PlaceOrder {
                 id,
-                offer: offer_id,
+                offer,
                 quantity,
                 leverage,
-            } => (id, offer_id, quantity, leverage),
+            } => (id, offer.id, quantity, leverage),
             TakerMessage::ContractSetupMsg(_) => {
                 tracing::error!("Unexpected message");
                 return;
@@ -150,7 +150,7 @@ impl Actor {
         tracing::info!(%peer_id, %quantity, %order_id, "Taker wants to place an order");
 
         // Reject the order if the offer cannot be found in the latest offers
-        let offer = match self.pick_offer(offer.id).await {
+        let offer = match self.pick_offer(offer_id).await {
             Ok(offer) => offer,
             Err(e) => {
                 tracing::warn!("Rejecting taker order because unable to pick offer: {e:#}");

daemon/src/order/protocol.rs
@@ -11,7 +11,7 @@ use maia_core::PartyParams;
 use maia_core::PunishParams;
 use model::Contracts;
 use model::Leverage;
-use model::Offer;
+use model::OfferId;
 use model::OrderId;
 use serde::Deserialize;
 use serde::Serialize;
@@ -22,11 +22,20 @@ use std::ops::RangeInclusive;
 pub(crate) enum TakerMessage {
     PlaceOrder {
         id: OrderId,
-        offer: Box<Offer>,
+        offer: Offer,
         quantity: Contracts,
         leverage: Leverage,
     },
     ContractSetupMsg(Box<SetupMsg>),
+}
+
+/// Identifies the offer which the taker used as a source to place the order.
+///
+/// We keep this nested structure (as opposed to sending the `OrderId` directly) so that we remain
+/// compatible with old taker versions who send a full offer.
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Offer {
+    pub id: OfferId,
 }
 
 #[derive(Debug, Serialize, Deserialize)]

daemon/src/order/taker.rs
@@ -2,6 +2,7 @@ use crate::command;
 use crate::oracle;
 use crate::oracle::NoAnnouncement;
 use crate::order::contract_setup;
+use crate::order::protocol;
 use crate::order::protocol::Decision;
 use crate::order::protocol::MakerMessage;
 use crate::order::protocol::SetupMsg;
@@ -140,7 +141,7 @@ impl Actor {
                 framed
                     .send(TakerMessage::PlaceOrder {
                         id: order_id,
-                        offer: Box::new(offer),
+                        offer: protocol::Offer { id: offer.id },
                         quantity,
                         leverage,
                     })