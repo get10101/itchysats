eec5985ec1f03cf80c3d11894d8e744e6ce80624
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2022-09-14 13:45:35 +1000

    chore(xtras): Remove unused address_map module

--- xtras/src/lib.rs
+++ xtras/src/lib.rs
@@ -1,5 +1,4 @@
 mod actor_name;
-pub mod address_map;
 pub mod handler_timeout;
 mod send_async_next;
 mod send_async_safe;
@@ -7,7 +6,6 @@ pub mod send_interval;
 pub mod supervisor;
 
 pub use actor_name::ActorName;
-pub use address_map::AddressMap;
 pub use handler_timeout::HandlerTimeoutExt;
 pub use send_async_next::SendAsyncNext;
 pub use send_async_safe::SendAsyncSafe;
