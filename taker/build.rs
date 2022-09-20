e1cc5491e7b837c32cf4623746e53204d95f0657
Mariusz Klochowicz <mariusz@klochowicz.com>
2022-09-20 11:58:52 +0930

    chore: Move build.rs frontend prep to relevant crates
    Daemon crate should not be concerned about frontend directories; this is the job
    of the binaries.

--- taker/build.rs
+++ taker/build.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    std::fs::create_dir_all("../maker-frontend/dist/maker")?;
     std::fs::create_dir_all("../taker-frontend/dist/taker")?;
     Ok(())
 }
